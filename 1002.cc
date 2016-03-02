248477ce0cc9dc06b0a773eda51acc9529da4c0c
Mourzoufle <Mourzoufle@163.com>
2016-03-02 16:03:23 +0800

    1002 - final

--- 1002.cc
+++ 1002.cc
@@ -6,7 +6,6 @@ int main() {
 	double *terms = new double[1024];
 	for (int i = 0; i < 1024; i++)
 		terms[i] = 0;
-
 	int num_nonzero = 0;
 	for (int i = 0; i < 2; i++) {
 		int num_term;
@@ -22,7 +21,6 @@ int main() {
 				num_nonzero--;
 		}
 	}
-
 	printf("%d", num_nonzero);
 	for (int i = 1023; i >= 0; i--)
 		if (terms[i] != 0)

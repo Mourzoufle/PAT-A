8a728369dc5c8948925e0638e0273d1e6c44e2e6
Mourzoufle <Mourzoufle@163.com>
2016-03-02 14:51:44 +0800

    1001 - final

--- 1001.cc
+++ 1001.cc
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <cstdio>
 #include <string>
 #include <algorithm>
 
@@ -6,23 +6,24 @@ using namespace std;
 
 int main() {
 	int a, b;
-	cin >> a >> b;
+	scanf("%d %d", &a, &b);
 	int sum = a + b;
 	bool is_neg = sum < 0;
 	if (is_neg)
 		sum = -sum;
-	string disp;
+	string disp;				// the reversed result
 	for (int i = 0; sum > 0; i++) {
-		if ((i > 0) && (i % 3 == 0))
+		if ((i > 0) && (i % 3 == 0))	// add comma when 3 digits read
 			disp += ',';
 		disp += '0' + sum % 10;
 		sum /= 10;
 	}
-	if (disp.length() == 0)
+	if (disp.empty())
 		disp = "0";
 	else if (is_neg)
 		disp += '-';
 	reverse(disp.begin(), disp.end());
-	cout << disp;
+	printf("%s", disp.c_str());
+
 	return 0;
 }

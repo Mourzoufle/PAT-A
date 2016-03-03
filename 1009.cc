9f8a1e30b2c08c27e88611cf62db96af56fe117e
Mourzoufle <Mourzoufle@163.com>
2016-03-04 00:42:42 +0800

    1009 - final

--- 1009.cc
+++ 1009.cc
@@ -1,41 +1,38 @@
-#include <iostream>
 #include <cstdio>
 
-using namespace std;
+const int MAX_TERM = 2001;
 
 int main() {
-	int num_a;
-	cin >> num_a;
-	int *expon_a = new int[num_a];
-	double *coeff_a = new double[num_a];
-	for (int i = 0; i < num_a; i++)
-		cin >> expon_a[i] >> coeff_a[i];
-	int num_b;
-	cin >> num_b;
-	int *expon_b = new int[num_b];
-	double *coeff_b = new double[num_b];
-	for (int i = 0; i < num_b; i++)
-		cin >> expon_b[i] >> coeff_b[i];
+	int nums_term[2];									// numbers of terms in two polynomials
+	int *expons[2];										// exponents in two polynomials
+	double *coeffs[2];									// coefficients in two polynomials
+	for (int i = 0; i < 2; i++) {
+		scanf("%d", &nums_term[i]);
+		expons[i] = new int[nums_term[i]];
+		coeffs[i] = new double[nums_term[i]];
+		for (int j = 0; j < nums_term[i]; j++)
+			scanf("%d %lf", &expons[i][j], &coeffs[i][j]);
+	}
 
-	double *res = new double[2048];
-	int num_nonzero = 0;
-	for (int i = 0; i < 2048; i++)
-		res[i] = 0;
-	for (int i = 0; i < num_a; i++) {
-		for (int j = 0; j < num_b; j++) {
-			int expon = expon_a[i] + expon_b[j];
-			double coeff = coeff_a[i] * coeff_b[j];
-			if (res[expon] == 0)
+	double *terms = new double[MAX_TERM];				// the result
+	int num_nonzero = 0;								// number of nonzero terms
+	for (int i = 0; i < MAX_TERM; i++)
+		terms[i] = 0;
+	for (int i = 0; i < nums_term[0]; i++) {
+		for (int j = 0; j < nums_term[1]; j++) {
+			int expon = expons[0][i] + expons[1][j];	// multiplication - addition in exponents
+			double coeff = coeffs[0][i] * coeffs[1][j];	// new coefficient
+			if (terms[expon] == 0)						// a nonzero term occurred
 				num_nonzero++;
-			res[expon] += coeff;
-			if (res[expon] == 0)
+			terms[expon] += coeff;
+			if (terms[expon] == 0)						// a nonzero terms becomes zero
 				num_nonzero--;
 		}
 	}
-	cout << num_nonzero;
-	for (int i = 2047; i >= 0; i--)
-		if (res[i] != 0)
-			printf(" %d %.1f", i, res[i]);
+	printf("%d", num_nonzero);
+	for (int i = MAX_TERM - 1; i >= 0; i--)
+		if (terms[i] != 0)
+			printf(" %d %.1lf", i, terms[i]);
 
 	return 0;
 }

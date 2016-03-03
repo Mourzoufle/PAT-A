51d26d558e76f8a86d1ba33667d0a6c5c2937c2c
Mourzoufle <Mourzoufle@163.com>
2016-03-04 00:25:36 +0800

    1008 - final

--- 1008.cc
+++ 1008.cc
@@ -1,21 +1,18 @@
-#include <iostream>
-
-using namespace std;
+#include <cstdio>
 
 int main() {
-	int cur = 0, time = 0;
-	int num;
-	cin >> num;
-	for (int i = 0; i < num; i++) {
+	int cur = 0, sum = 0, num_request;		// current floor; total time; number of requests to this elevator
+	scanf("%d", &num_request);
+	for (int i = 0; i < num_request; i++) {
 		int next;
-		cin >> next;
-		if (next > cur)
-			time += 6 * (next - cur);
-		else
-			time += 4 * (cur - next);
+		scanf("%d", &next);
+		if (next > cur)						// up
+			sum += 6 * (next - cur);
+		else								// down
+			sum += 4 * (cur - next);
 		cur = next;
 	}
-	cout << time + 5 * num;
+	printf("%d", sum + 5 * num_request);	// wait for 5 seconds for each request
 
 	return 0;
 }

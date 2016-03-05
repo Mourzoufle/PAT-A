1e4cc9ee138b9d3e76739f3c1ac31722e36b5b09
Mourzoufle <Mourzoufle@163.com>
2016-03-05 16:30:18 +0800

    1013 - final

--- 1013.cc
+++ 1013.cc
@@ -1,42 +1,45 @@
-#include <iostream>
+#include <cstdio>
 
-using namespace std;
-
-void merge(int *sets, int src, int dst) {
-	while (sets[src] != src)
-		src = sets[src];
-	while (sets[dst] != dst) {
-		int tmp = sets[dst];
-		sets[dst] = src;
-		dst = tmp;
+/* Union-Find Sets - merge the sets that a and b belong to */
+void merge(int *roots, int a, int b) {
+	int root = a;
+	while (roots[root] != root)									// find the root of a as the new root of two sets
+		root = roots[root];
+	while (roots[a] != root) {									// path compression of a and b
+		int tmp = roots[a];
+		roots[a] = root;
+		a = tmp;
 	}
-	sets[dst] = src;
+	while (roots[b] != root) {
+		int tmp = roots[b];
+		roots[b] = root;
+		b = tmp;
+	}
 }
 
 int main() {
-	int num_city, num_road, num_check;
-	cin >> num_city >> num_road >> num_check;
+	int num_city, num_road, num_check;							// number of cities, roads, and cities to be checked
+	scanf("%d %d %d", &num_city, &num_road, &num_check);
 	int **roads = new int *[num_road];
 	for (int i = 0; i < num_road; i++) {
 		roads[i] = new int[2];
-		cin >> roads[i][0] >> roads[i][1];
+		scanf("%d %d", &roads[i][0], &roads[i][1]);
 	}
-	int *sets = new int[num_city];
-	for (int r = 0; r < num_check; r++) {
-		int target;
-		cin >> target;
-		for (int i = 0; i < num_city; i++)
-			sets[i] = i;
-		for (int i = 0; i < num_road; i++)
-			if ((roads[i][0] != target) && (roads[i][1] != target))
-				merge(sets, roads[i][0] - 1, roads[i][1] - 1);
-		int num_repair = -2;
-		for (int i = 0; i < num_city; i++)
-			if (sets[i] == i)
-				num_repair++;
-		if (num_repair < 0)
-			num_repair = 0;
-		cout << num_repair << endl;
+
+	int *roots = new int[num_city];								// the root city of each city
+	for (int i = 0; i < num_check; i++) {
+		int city;
+		scanf("%d", &city);
+		for (int j = 0; j < num_city; j++)						// all cities are separated sets
+			roots[j] = j;
+		for (int j = 0; j < num_road; j++)
+			if ((roads[j][0] != city) && (roads[j][1] != city))	// drop the roads connected with the city under checking
+				merge(roots, roads[j][0] - 1, roads[j][1] - 1);
+		int num_set = 0;
+		for (int j = 0; j < num_city; j++)						// number of roots indicates number of sets
+			if (roots[j] == j)
+				num_set++;
+		printf("%d\n", num_set > 1 ? num_set - 2 : 0);			// if only one set (beside of the city under checking) - no road needed
 	}
 
 	return 0;

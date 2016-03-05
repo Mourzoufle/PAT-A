dd2da6f0ae451e39be197c4d4096250d13955cfb
Mourzoufle <Mourzoufle@163.com>
2016-03-06 03:43:44 +0800

    1065

---
+++ 1065.cc
@@ -0,0 +1,24 @@
+#include <cstdio>
+
+int main() {
+	int num_case;
+	scanf("%d", &num_case);
+	for (int i = 1; i <= num_case; i++) {
+		long long a, b, c;
+		scanf("%lld %lld %lld", &a, &b, &c);
+		long long sum = a + b;
+		bool grt;
+		if ((a > 0) && (b > 0) && (sum <= 0))
+			grt = true;
+		else if ((a < 0) && (b < 0) && (sum >= 0))
+			grt = false;
+		else
+			grt = sum > c;
+		if (grt)
+			printf("Case #%d: true\n", i);
+		else
+			printf("Case #%d: false\n", i);
+	}
+
+	return 0;
+}

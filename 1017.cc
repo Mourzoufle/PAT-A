d72f88a1c6a0ac18250e34beb1a024a7163bd2cc
Mourzoufle <Mourzoufle@163.com>
2016-03-06 22:29:17 +0800

    1017 - final

--- 1017.cc
+++ 1017.cc
@@ -4,11 +4,12 @@
 using namespace std;
 
 struct Customer {
-	int arriving_time;
-	int serving_time;
+	int time_arrive;																					// arriving time
+	int time_serve;																						// time needed for serving this customer - in seconds
 };
 
-bool compare(const Customer &a, const Customer&b) { return a.arriving_time < b.arriving_time; };
+/* compare fuction for sorting customers - according to their arriving time */
+bool cmp(const Customer &a, const Customer &b) { return a.time_arrive < b.time_arrive; };
 
 int main() {
 	int num_customer, num_window;
@@ -16,32 +17,31 @@ int main() {
 	Customer *customers = new Customer[num_customer];
 	for (int i = 0; i < num_customer; i++) {
 		int hour, minute, second;
-		scanf("%d:%d:%d %d", &hour, &minute, &second, &customers[i].serving_time);
-		customers[i].arriving_time = hour * 3600 + minute * 60 + second;
-		if (customers[i].serving_time > 60)
-			customers[i].serving_time = 60;
+		scanf("%d:%d:%d %d", &hour, &minute, &second, &customers[i].time_serve);
+		customers[i].time_arrive = hour * 3600 + minute * 60 + second;									// convert time to seconds
+		customers[i].time_serve = customers[i].time_serve > 60 ? 3600 : customers[i].time_serve * 60;	// serving time may not exceed an hour
 	}
-	sort(customers, customers + num_customer, compare);
+	sort(customers, customers + num_customer, cmp);
 
-	int *windows = new int[num_window];
+	int *windows = new int[num_window];																	// available time of each window
 	for (int i = 0; i < num_window; i++)
-		windows[i] = 28800;
-	int sum = 0, num_valid = 0;
+		windows[i] = 28800;																				// open at 8:00
+	double time_wait = 0, num_valid = 0;																// total waiting time; number of customers that actually get served
 	for (int i = 0; i < num_customer; i++) {
-		if (customers[i].arriving_time > 61200)
+		if (customers[i].time_arrive > 61200)															// customers come after 17:00 will not get served
 			break;
-		int idx = 0;
+		int cur_window = 0;
 		for (int j = 1; j < num_window; j++)
-			if (windows[idx] > windows[j])
-				idx = j;
-		if (windows[idx] > customers[i].arriving_time)
-			sum += windows[idx] - customers[i].arriving_time;
+			if (windows[j] < windows[cur_window])														// the current customer will choose the first available window
+				cur_window = j;
+		if (windows[cur_window] > customers[i].time_arrive)												// the customer comes before the window is available - has to wait
+			time_wait += windows[cur_window] - customers[i].time_arrive;
 		else
-			windows[idx] = customers[i].arriving_time;
-		windows[idx] += customers[i].serving_time * 60;
+			windows[cur_window] = customers[i].time_arrive;												// or reset the time this window starts to serve
+		windows[cur_window] += customers[i].time_serve;													// the time this window is available again
 		num_valid++;
 	}
-	printf("%.1f", sum / 60.0 / num_valid);
+	printf("%.1lf", time_wait / 60 / num_valid);														// print the average waiting time IN MINUTES
 
 	return 0;
 }

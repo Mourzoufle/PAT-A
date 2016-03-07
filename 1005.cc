14c42f86897b9bae3bb72a8347db005b172d45d4
Mourzoufle <Mourzoufle@163.com>
2016-03-07 20:20:30 +0800

    1005 - final

--- 1005.cc
+++ 1005.cc
@@ -23,13 +23,12 @@ int main() {
 	for (int i = 0; num[i] != '\0'; i++)
 		sum += num[i] - '0';
 
-	vector<int> digits;	// the digits - from the lowest to the highest
-	while (sum > 0) {
+	vector<int> digits;	// the digits - reversed order
+	while (sum >= 10) {
 		digits.push_back(sum % 10);
 		sum /= 10;
 	}
-	if (digits.empty())	// if the sum is zero
-		digits.push_back(0);
+	digits.push_back(sum);
 	printf("%s", SPELLS[digits.back()]);
 	for (int i = digits.size() - 2; i >= 0; i--)
 		printf(" %s", SPELLS[digits[i]]);

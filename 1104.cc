1b240ff98e81af0eb5aaf30ea7c475ba96c0c0ba
Mourzoufle <Mourzoufle@163.com>
2016-03-08 00:32:13 +0800

    1104

---
+++ 1104.cc
@@ -0,0 +1,15 @@
+#include <cstdio>
+
+int main() {
+	int num_num;
+	scanf("%d", &num_num);
+	double sum = 0;
+	for (int i = 0; i < num_num; i++) {
+		double num;
+		scanf("%lf", &num);
+		sum += num * (i + 1) * (num_num - i);
+	}
+	printf("%.2lf", sum);
+
+	return 0;
+}

106718119e9bde6c7fc7968c25fd05570026e168
Mourzoufle <Mourzoufle@163.com>
2016-03-07 23:30:29 +0800

    1021.cc

--- 1021.cc
+++ 1021.cc
@@ -5,89 +5,77 @@
 
 using namespace std;
 
-void merge(int *nodes, int src, int dst) {
-	while (nodes[dst] != dst)
-		dst = nodes[dst];
-	while (nodes[src] != src) {
-		int tmp = nodes[src];
-		nodes[src] = dst;
-		src = tmp;
+/* Union-Find Sets - merge the sets that a and b belong to */
+void merge(int *roots, int a, int b) {
+	int root = a;
+	while (roots[root] != root)									// find the root of a as the new root of two sets
+		root = roots[root];
+	while (roots[a] != root) {									// path compression of a and b
+		int tmp = roots[a];
+		roots[a] = root;
+		a = tmp;
 	}
-	nodes[src] = dst;
+	while (roots[b] != root) {
+		int tmp = roots[b];
+		roots[b] = root;
+		b = tmp;
+	}
 }
 
-int get_part(int *heads, int *tails, int num_node) {
-	int *nodes = new int[num_node];
-	for (int i = 0; i < num_node; i++)
-		nodes[i] = i;
-	for (int i = 0; i < num_node - 1; i++)
-		merge(nodes, heads[i], tails[i]);
-	int num_part = 0;
-	for (int i = 0; i < num_node; i++)
-		if (nodes[i] == i)
-			num_part++;
-
-	return num_part;
-}
-
-set<int>* find_root(int *heads, int *tails, int num_node, int src) {
-	set<int> *roots = new set<int>;
+/* BFS - find the deepest roots from src, and add them to the ones that are already found */
+void get_roots(int **roads, int num_node, int src, set<int> &roots) {
 	bool *visit = new bool[num_node];
-	for (int i = 0; i < num_node; i++)
+	for (int i = 0; i < num_node; i++)							// every node is unvisited
 		visit[i] = false;
-	queue<int> nodes, lengths;
-	nodes.push(src);
-	lengths.push(0);
-	int max_length = 0;
-	while (!nodes.empty()) {
-		int cur_node = nodes.front();
-		int cur_length = lengths.front();
-		nodes.pop();
-		lengths.pop();
-		bool end = true;
-		for (int i = 0; i < num_node - 1; i++) {
-			if (visit[heads[i]] || visit[tails[i]])
-				continue;
-			if ((heads[i] != cur_node) && (tails[i] != cur_node))
-				continue;
-			nodes.push(heads[i] == cur_node ? tails[i] : heads[i]);
-			lengths.push(cur_length + 1);
-			end = false;
-		}
-		visit[cur_node] = true;
-		if (end) {
-			if (cur_length > max_length) {
-				roots->clear();
-				max_length = cur_length;
+	set<int> cur_roots;											// deepest roots found at this time
+	queue<int> queue;											// use BFS
+	queue.push(src);
+	while (!queue.empty()) {
+		cur_roots.clear();										// nodes with larger length exist, clear the current result
+		int cur_size = queue.size();
+		for (int i = 0; i < cur_size; i++) {
+			int node = queue.front();
+			visit[node] = true;
+			cur_roots.insert(node);
+			for (int j = 0; j < num_node - 1; j++) {			// nodes connected to the current node and unvisited are counted as deeper nodes
+				if ((roads[j][0] == node) && !visit[roads[j][1]])
+					queue.push(roads[j][1]);
+				else if ((roads[j][1] == node) && !visit[roads[j][0]])
+					queue.push(roads[j][0]);
 			}
-			if (cur_length == max_length)
-				roots->insert(cur_node);
+			queue.pop();
 		}
 	}
+	roots.insert(cur_roots.begin(), cur_roots.end());
 	delete[] visit;
-
-	return roots;
 }
 
 int main() {
 	int num_node;
 	scanf("%d", &num_node);
-	int *heads = new int[num_node - 1];
-	int *tails = new int[num_node - 1];
+	int *roots = new int[num_node];								// the root node of each node
+	for (int i = 0; i < num_node; i++)
+		roots[i] = i;											// all nodes are separated sets
+	int **roads = new int *[num_node - 1];						
 	for (int i = 0; i < num_node - 1; i++) {
-		scanf("%d %d", &heads[i], &tails[i]);
-		heads[i]--;
-		tails[i]--;
+		roads[i] = new int[2];
+		scanf("%d %d", &roads[i][0], &roads[i][1]);
+		merge(roots, --roads[i][0], --roads[i][1]);
 	}
-	int num_part = get_part(heads, tails, num_node);
-	if (num_part != 1) {
-		printf("Error: %d components", num_part);
+
+	int num_component = 0;
+	for (int i = 0; i < num_node; i++)
+		if (roots[i] == i)										// number of roots indicates number of components
+			num_component++;
+	if (num_component > 1) {									// not a connected graph
+		printf("Error: %d components", num_component);
 		return 0;
 	}
-	set<int> *init = find_root(heads, tails, num_node, 0);
-	set<int> *final = find_root(heads, tails, num_node, *(init->begin()));
-	final->insert(init->begin(), init->end());
-	for (set<int>::iterator iter = final->begin(); iter != final->end(); iter++)
+
+	set<int> deepest_roots;
+	get_roots(roads, num_node, 0, deepest_roots);				// NOTE: the deepest roots needed to be completely found after searching twice
+	get_roots(roads, num_node, *(deepest_roots.begin()), deepest_roots);
+	for (set<int>::iterator iter = deepest_roots.begin(); iter != deepest_roots.end(); iter++)
 		printf("%d\n", (*iter) + 1);
 
 	return 0;

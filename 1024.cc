99022de8103249a02b695836eddb8def6098a477
Mourzoufle <Mourzoufle@163.com>
2016-03-09 23:10:25 +0800

    1024 - final

--- 1024.cc
+++ 1024.cc
@@ -4,40 +4,41 @@
 
 using namespace std;
 
-string add(string a, string b) {
+/* get the sum of number representation a and b - the result may be out of the range of long long integer, use string instead */
+string get_sum(string a, string b) {
+	string sum;
 	for (int i = 0; i < a.size(); i++) {
 		a[i] -= '0';
 		b[i] -= '0';
 	}
-	string res;
 	bool carry = false;
-	for (int i = a.size() - 1; i >= 0; i--) {
-		int num = a[i] + b[i] + (carry ? 1 : 0);
+	for (int i = a.size() - 1; i >= 0; i--) {			// ASSUMED a and b have equal length
+		int num = a[i] + b[i] + (carry ? 1 : 0);		// current digit in a and b + carry from the lower digit
 		carry = num > 9;
-		res += (num % 10) + '0';
+		sum += (num % 10) + '0';
 	}
 	if (carry)
-		res += '1';
-	reverse(res.begin(), res.end());
+		sum += '1';
+	reverse(sum.begin(), sum.end());					// the representation of the sum is reversed
 
-	return res;
+	return sum;
 }
 
 int main() {
-	char str[16];
-	int num_step;
-	scanf("%s %d", str, &num_step);
-	string num = str;
-	for (int i = 0; i < num_step; i++) {
-		string rev_num = num;
-		reverse(rev_num.begin(), rev_num.end());
-		if (num == rev_num) {
-			printf("%s\n%d", num.c_str(), i);
+	char num[16];
+	int max_step;
+	scanf("%s %d", num, &max_step);
+	string original_num = num;
+	for (int i = 0; i < max_step; i++) {
+		string reversed_num = original_num;
+		reverse(reversed_num.begin(), reversed_num.end());
+		if (original_num == reversed_num) {				// got a palindromic number
+			printf("%s\n%d", original_num.c_str(), i);
 			return 0;
 		}
-		num = add(num, rev_num);
+		original_num = get_sum(original_num, reversed_num);
 	}
-	printf("%s\n%d", num.c_str(), num_step);
+	printf("%s\n%d", original_num.c_str(), max_step);	// if not get a palindromic number at the max step, just print the result
 
 	return 0;
 }

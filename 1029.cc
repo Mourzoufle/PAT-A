a752e4b1f022ea4ca0c4242efb852269e5b86170
Mourzoufle <Mourzoufle@163.com>
2016-03-11 13:58:13 +0800

    1029 - final

--- 1029.cc
+++ 1029.cc
@@ -1,27 +1,25 @@
 #include <cstdio>
 
-using namespace std;
-
 int main(){
-	int num[2];
+	int nums_num[2];																									// numbers of numbers in each array
 	long *nums[2];
 	for (int i = 0; i < 2; i++) {
-		scanf("%d", &num[i]);
-		nums[i] = new long[num[i]];
-		for (int j = 0; j < num[i]; j++)
+		scanf("%d", &nums_num[i]);
+		nums[i] = new long[nums_num[i]];
+		for (int j = 0; j < nums_num[i]; j++)
 			scanf("%ld", &nums[i][j]);
 	}
-	int idx[2] = {0, 0}, mid = (num[0] + num[1] - 1) >> 1;
+	int indices[2] = {0, 0}, mid = (nums_num[0] + nums_num[1] - 1) >> 1;												// current indices of number processed in each array; index of the median (in all the numbers)
 	for (int i = 0; i < mid; i++) {
-		if ((idx[1] == num[1]) || ((idx[0] < num[0]) && (nums[0][idx[0]] < nums[1][idx[1]])))
-			idx[0]++;
+		if ((indices[1] == nums_num[1]) || ((indices[0] < nums_num[0]) && (nums[0][indices[0]] < nums[1][indices[1]])))	// the smaller number is processed, index in the corresponding array + 1
+			indices[0]++;
 		else
-			idx[1]++;
+			indices[1]++;
 	}
-	if ((idx[1] == num[1]) || ((idx[0] < num[0]) && (nums[0][idx[0]] < nums[1][idx[1]])))
-		printf("%ld", nums[0][idx[0]]);
+	if ((indices[1] == nums_num[1]) || ((indices[0] < nums_num[0]) && (nums[0][indices[0]] < nums[1][indices[1]])))		// finally, the smaller one is the median
+		printf("%ld", nums[0][indices[0]]);
 	else
-		printf("%ld", nums[1][idx[1]]);
+		printf("%ld", nums[1][indices[1]]);
 
 	return 0;
 }

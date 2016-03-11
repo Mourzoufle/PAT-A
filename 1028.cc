9be35411ebd252fc541e3270959a03d6a984d399
Mourzoufle <Mourzoufle@163.com>
2016-03-11 13:50:52 +0800

    1028 - final

--- 1028.cc
+++ 1028.cc
@@ -1,50 +1,50 @@
 #include <cstdio>
-#include <string>
+#include <cstring>
 #include <algorithm>
 
 using namespace std;
 
 struct Person {
 	int id;
-	string name;
-	int score;
+	char name[16];
+	int grade;
 };
 
-bool compare_id(const Person &a, const Person &b) { return a.id < b.id; };
+/* compare fuction for sorting records - according to id */
+bool cmp_id(const Person &a, const Person &b) { return a.id < b.id; };
 
-bool compare_name(const Person &a, const Person &b) {
-	if (a.name != b.name)
-		return a.name < b.name;
+/* compare fuction for sorting records - according to name, then id */
+bool cmp_name(const Person &a, const Person &b) {
+	if (strcmp(a.name, b.name) != 0)
+		return strcmp(a.name, b.name) < 0;
 	return a.id < b.id;
 }
 
-bool compare_score(const Person &a, const Person &b) {
-	if (a.score != b.score)
-		return a.score < b.score;
+/* compare fuction for sorting records - according to higher grade, then id */
+bool cmp_grade(const Person &a, const Person &b) {
+	if (a.grade != b.grade)
+		return a.grade < b.grade;
 	return a.id < b.id;
 }
 
 int main() {
-	int num_person, idx;
-	scanf("%d %d", &num_person, &idx);
+	int num_person, idx_sort;
+	scanf("%d %d", &num_person, &idx_sort);
 	Person *people = new Person[num_person];
-	for (int i = 0; i < num_person; i++) {
-		char name[16];
-		scanf("%d %s %d", &people[i].id, name, &people[i].score);
-		people[i].name = name;
-	}
-	switch (idx) {
+	for (int i = 0; i < num_person; i++)
+		scanf("%d %s %d", &people[i].id, people[i].name, &people[i].grade);
+	switch (idx_sort) {
 	case 1:
-		sort(people, people + num_person, compare_id);
+		sort(people, people + num_person, cmp_id);
 		break;
 	case 2:
-		sort(people, people + num_person, compare_name);
+		sort(people, people + num_person, cmp_name);
 		break;
 	case 3:
-		sort(people, people + num_person, compare_score);
+		sort(people, people + num_person, cmp_grade);
 	}
 	for (int i = 0; i < num_person; i++)
-		printf("%06d %s %d\n", people[i].id, people[i].name.c_str(), people[i].score);
+		printf("%06d %s %d\n", people[i].id, people[i].name, people[i].grade);
 
 	return 0;
 }

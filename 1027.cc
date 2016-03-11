24c158f48aa3185cca12b0aa6317d68642baa456
Mourzoufle <Mourzoufle@163.com>
2016-03-11 13:35:44 +0800

    1027 - final

--- 1027.cc
+++ 1027.cc
@@ -1,18 +1,16 @@
 #include <cstdio>
 
-using namespace std;
-
 int main() {
 	printf("#");
 	for (int i = 0; i < 3; i++) {
 		int num;
 		scanf("%d", &num);
-		if (num >= 130)
+		if (num >= 130)							// the first digit > 9
 			printf("%c", num / 13 + 'A' - 10);
 		else
 			printf("%d", num / 13);
 		num %= 13;
-		if (num >= 10)
+		if (num >= 10)							// the second digit > 9
 			printf("%c", num + 'A' - 10);
 		else
 			printf("%d", num);

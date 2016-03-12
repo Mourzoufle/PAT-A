89b6e61a54391b94ebf00eeba4443a13609ed0c8
Mourzoufle <Mourzoufle@163.com>
2016-03-12 19:03:58 +0800

    1031 - final

--- 1031.cc
+++ 1031.cc
@@ -1,21 +1,18 @@
 #include <cstdio>
-#include <string>
-
-using namespace std;
+#include <cstring>
 
 int main() {
-	char data[128];
-	scanf("%s", data);
-	string str = data;
-	int height = (str.size() + 2) / 3;
-	int width = str.size() - (height << 1) + 2;
-	for (int i = 1; i < height; i++) {
-		printf("%c", str[i - 1]);
+	char str[128];
+	scanf("%s", str);
+	int length = strlen(str);
+	int height = (length + 2) / 3, width = length + 2 - (height * 2);	// 2 * height + width - 2 = length of string
+	for (int i = 0; i < height - 1; i++) {
+		printf("%c", str[i]);
 		for (int j = 2; j < width; j++)
 			printf(" ");
-		printf("%c\n", str[str.size() - i]);
+		printf("%c\n", str[length - i - 1]);
 	}
-	for (int i = height - 1; i < str.size() + 1 - height; i++)
+	for (int i = height - 1; i < length + 1 - height; i++)
 		printf("%c", str[i]);
 
 	return 0;

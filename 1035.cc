dd43d67824e90354c16a8ffdd2340dbcf165d1a6
Mourzoufle <Mourzoufle@163.com>
2016-03-16 22:33:58 +0800

    1035 - final

--- 1035.cc
+++ 1035.cc
@@ -3,13 +3,14 @@
 
 using namespace std;
 
-struct Record {
+struct Account {
 	char id[16];
 	char password[16];
 };
 
+/* check if the given password needs to be modified, and do it */
 bool modify(char *password) {
-	bool modify = false;
+	bool modify = false;			// flag indicating if the password is modified
 	for (int i = 0; password[i] != '\0'; i++) {
 		if (password[i] == '1') {
 			password[i] = '@';
@@ -33,25 +34,25 @@ bool modify(char *password) {
 }
 
 int main() {
-	int num_record, num_modify = 0;
-	scanf("%d", &num_record);
-	Record *records = new Record[num_record];
-	vector<int> modified_indices;
-	for (int i = 0; i < num_record; i++) {
-		scanf("%s %s", &records[i].id, &records[i].password);
-		if (modify(records[i].password))
-			modified_indices.push_back(i);
+	int num_account, num_modify = 0;
+	scanf("%d", &num_account);
+	Account *accounts = new Account[num_account];
+	vector<int> indices_modified;	// the indices of modified accounts
+	for (int i = 0; i < num_account; i++) {
+		scanf("%s %s", &accounts[i].id, &accounts[i].password);
+		if (modify(accounts[i].password))
+			indices_modified.push_back(i);
 	}
-	if (modified_indices.empty()) {
-		if (num_record > 1)
-			printf("There are %d accounts and no account is modified", num_record);
+	if (indices_modified.empty()) {	// no account is modified
+		if (num_account > 1)
+			printf("There are %d accounts and no account is modified", num_account);
 		else 
 			printf("There is 1 account and no account is modified");
 	}
 	else {
-		printf("%d\n", modified_indices.size());
-		for (int i = 0; i < modified_indices.size(); i++)
-			printf("%s %s\n", records[modified_indices[i]].id, records[modified_indices[i]].password);
+		printf("%d\n", indices_modified.size());
+		for (int i = 0; i < indices_modified.size(); i++)
+			printf("%s %s\n", accounts[indices_modified[i]].id, accounts[indices_modified[i]].password);
 	}
 
 	return 0;

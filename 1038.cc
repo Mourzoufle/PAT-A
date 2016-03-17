8e4864470b379fe944c5d726f9ccbc0f6c1b9e06
Mourzoufle <Mourzoufle@163.com>
2016-03-17 11:37:58 +0800

    1038 - final

--- 1038.cc
+++ 1038.cc
@@ -4,13 +4,15 @@
 
 using namespace std;
 
-bool compare(const string &a, const string &b) { return a + b < b + a; }
+/* compare fuction for segments - if the combining segments as {ab} is smaller than {ba}, then sort a in the front of b */
+bool cmp(const string &a, const string &b) { return a + b < b + a; }
 
-int idx_nonzero(string num) {
+/* find the index of the first nonzero number in the segment */
+int get_idx_nonzero(string num) {
 	for (int i = 0; i < num.size(); i++)
 		if (num[i] != '0')
 			return i;
-	return num.size();
+	return -1;												// if all the numbers in the segments are zeros, return -1
 }
 
 int main() {
@@ -22,21 +24,21 @@ int main() {
 		scanf("%s", segment);
 		segments[i] = segment;
 	}
-	sort(segments, segments + num_segment, compare);
+	sort(segments, segments + num_segment, cmp);
 
-	bool print = false;
+	bool print = false;										// flag indicating if there is a nonzero number is found and printed
 	for (int i = 0; i < num_segment; i++) {
-		if (print)
+		if (print)											// if there is already a nonzero number printed, just print this segment
 			printf("%s", segments[i].c_str());
 		else {
-			int idx = idx_nonzero(segments[i]);
-			if (idx < segments[i].size()) {
-				printf("%s", segments[i].substr(idx).c_str());
+			int idx_nonzero = get_idx_nonzero(segments[i]);	// or find the first nonzero number in this segment
+			if (idx_nonzero >= 0) {							// if a nonzero number is found, then print part of the segment starting from the number
+				printf("%s", segments[i].substr(idx_nonzero).c_str());
 				print = true;
 			}
 		}
 	}
-	if (!print)
+	if (!print)												// if all the numbers in the segments are zeros, just print 0
 		printf("0");
 
 	return 0;

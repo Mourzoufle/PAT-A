7b50d3935808c6b65af08f3280f70a374cd25147
Mourzoufle <Mourzoufle@163.com>
2016-03-19 17:24:39 +0800

    1042 - final

--- 1042.cc
+++ 1042.cc
@@ -1,32 +1,28 @@
-#include <iostream>
 #include <cstdio>
 
-using namespace std;
-
-const char TYPES[] = {'S', 'H', 'C', 'D'}; 
+const char TYPES[8] = "SHCDJ";								// types of cards
+const int NUM_CARD = 54;									// number of cards
 
 int main() {
-	int **cards = new int *[2];
-	cards[0] = new int[54];
-	cards[1] = new int[54];
-	for (int i = 0; i < 54; i++)
-		cards[0][i] = i;
-	int iter;
-	int *order = new int[54];
-	cin >> iter;
-	for (int i = 0; i < 54; i++)
-		cin >> order[i];
-	for (int r = 0; r < iter; r++)
-		for (int i = 0; i < 54; i++)
-			cards[1 - r % 2][order[i] - 1] = cards[r % 2][i];
-	for (int i = 0; i < 54; i++) {
+	int **cards = new int *[2];								// the current order of the cards and the next order after shuffling
+	for (int i = 0; i < 2; i++)
+		cards[i] = new int[NUM_CARD];
+	int num_repeat;											// number of repeating times
+	scanf("%d", &num_repeat);
+	int *order = new int[NUM_CARD];							// the given order of shuffling
+	for (int i = 0; i < NUM_CARD; i++) {
+		cards[0][i] = i;									// the initial order
+		scanf("%d", &order[i]);
+		order[i]--;
+	}
+
+	for (int i = 0; i < num_repeat; i++)
+		for (int j = 0; j < NUM_CARD; j++)
+			cards[1 - i % 2][order[j]] = cards[i % 2][j];	// for odd-numbered shuffling, the new order of cards are stored in cards[1], otherwise in cards[0]
+	for (int i = 0; i < NUM_CARD; i++) {
 		if (i > 0)
 			printf(" ");
-		int card = cards[iter % 2][i];
-		if (card > 51)
-			printf("J%d", card - 51);
-		else
-			printf("%c%d", TYPES[card / 13], card % 13 + 1);
+		printf("%c%d", TYPES[cards[num_repeat % 2][i] / 13], cards[num_repeat % 2][i] % 13 + 1);
 	}
 
 	return 0;

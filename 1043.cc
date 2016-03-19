d1b7791ae31cd965b9522e9cc710ba9382f019a5
Mourzoufle <Mourzoufle@163.com>
2016-03-19 18:12:20 +0800

    1043 - final

--- 1043.cc
+++ 1043.cc
@@ -1,43 +1,46 @@
-#include <iostream>
-#include <stack>
+#include <cstdio>
+#include <vector>
 
 using namespace std;
 
-bool travel(bool is_mirror, int *preorder, int head, int tail, stack<int> &order) {
-	order.push(preorder[head]);
-	int right = head;
-	while (++right <= tail)
-		if (!(is_mirror^(preorder[head] > preorder[right])))
+/* check if the given preorder (from head to tail) is the preorder of a BST or a mirror of BST, and generate the postorder for it */
+bool is_bst(bool is_mirror, int *preorder, int head, int tail, vector<int> &postorder) {
+	postorder.push_back(preorder[head]);							// the root is the first node in the preorder
+	int head_right = head;											// the head of the right subtree
+	while (++head_right <= tail)
+		if (!(is_mirror^(preorder[head] > preorder[head_right])))	// for a BST tree (is_mirror = 0), find the first node not less than the head; otherwise find the first less node
 			break;
-	for (int i = right; i <= tail; i++)
+	for (int i = head_right; i <= tail; i++)						// for a BST tree, check if there is any node in the right subtree that less than head; otherwise check if all of them are less
 		if (is_mirror^(preorder[head] > preorder[i]))
 			return false;
-	if (right <= tail)
-		if (!travel(is_mirror, preorder, right, tail, order))
+	if (head_right <= tail)											// if current node has right child, check on it
+		if (!is_bst(is_mirror, preorder, head_right, tail, postorder))
 			return false;
-	if (head <= right - 2)
-		if (!travel(is_mirror, preorder, head + 1, right - 1, order))
+	if (head <= head_right - 2)										// if current node has left child, check on it
+		if (!is_bst(is_mirror, preorder, head + 1, head_right - 1, postorder))
 			return false;
-
 	return true;
 }
 
 int main() {
 	int num_node;
-	cin >> num_node;
+	scanf("%d", &num_node);
 	int *preorder = new int[num_node];
 	for (int i = 0; i < num_node; i++)
-		cin >> preorder[i];
-	stack<int> postorder;
-	if (!travel(preorder[0] <= preorder[1], preorder, 0, num_node - 1, postorder))
-		cout << "NO";
-	else {
-		cout << "YES" << endl << postorder.top();
-		postorder.pop();
-		while (postorder.size() > 0) {
-			cout << " " << postorder.top();
-			postorder.pop();
-		}
+		scanf("%d", &preorder[i]);
+
+	vector<int> postorder;											// the postorder of the tree
+	if (!is_bst(false, preorder, 0, num_node - 1, postorder)) {		// if it is impossible to be a BST
+		postorder.clear();											// ...then clear the generated postorder and check if it is a mirror of BST
+		if (!is_bst(true, preorder, 0, num_node - 1, postorder))
+			postorder.clear();
+	}
+	if (postorder.empty())											// the postorder is cleared twice, not a BST or its mirror
+		printf("NO");
+	else {															// or print the postorder
+		printf("YES\n%d", postorder.back());
+		for (int i = postorder.size() - 2; i >= 0; i--)
+			printf(" %d", postorder[i]);
 	}
 
 	return 0;

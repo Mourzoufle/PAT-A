c7fe2250258f508d43d851160b1729c1a29a5d0b
Mourzoufle <Mourzoufle@163.com>
2016-03-20 00:53:14 +0800

    1045 - final

--- 1045.cc
+++ 1045.cc
@@ -1,36 +1,34 @@
-#include <iostream>
-
-using namespace std;
+#include <cstdio>
 
 int main() {
-	int num_color, num_favor, size_stripe;
-	cin >> num_color >> num_favor;
-	int *order = new int[num_color + 1];
-	for (int i = 1; i <= num_color; i++)
+	int num_color, num_favor, num_piece;
+	scanf("%d %d", &num_color, &num_favor);
+	int *order = new int[num_color];													// order of favorite colors - for colors not in the favorite list, it is -1
+	for (int i = 0; i <= num_color; i++)
 		order[i] = -1;
 	for (int i = 0; i < num_favor; i++) {
 		int color;
-		cin >> color;
-		order[color] = i;
+		scanf("%d", &color);
+		order[--color] = i;
 	}
-	cin >> size_stripe;
-	int *stripe = new int[size_stripe];
-	for (int i = 0; i < size_stripe; i++)
-		cin >> stripe[i];
+	scanf("%d", &num_piece);
+	int *pieces = new int[num_piece];
+	for (int i = 0; i < num_piece; i++)
+		scanf("%d", &pieces[i]);
 
-	int *lengths = new int[size_stripe];
+	int *lengths = new int[num_piece];													// the maximum length of favorite stripe that can be obtained via cutting at each piece
 	int max_length = 0;
-	for (int i = 0; i < size_stripe; i++) {
+	for (int i = 0; i < num_piece; i++) {
 		lengths[i] = 0;
-		if (order[stripe[i]] < 0)
+		if (order[--pieces[i]] < 0)														// this piece is not in a favorite color - no need to calculate the maxmum length
 			continue;
 		for (int j = 0; j < i; j++)
-			if ((lengths[j] > lengths[i]) && (order[stripe[j]] <= order[stripe[i]]))
+			if ((order[pieces[j]] <= order[pieces[i]]) && (lengths[j] > lengths[i]))	// same as calculating the length of the longest nondecreasing subsequence
 				lengths[i] = lengths[j];
 		if (++lengths[i] > max_length)
 			max_length = lengths[i];
 	}
-	cout << max_length;
+	printf("%d", max_length);
 
 	return 0;
 }

2c8cd070256deac6446e37f560a67ee2434579bd
Mourzoufle <Mourzoufle@163.com>
2016-03-21 11:20:27 +0800

    1046 - final

--- 1046.cc
+++ 1046.cc
@@ -1,26 +1,24 @@
-#include <iostream>
-
-using namespace std;
+#include <cstdio>
 
 int main() {
-	int num_distance, num_query;
-	cin >> num_distance;
-	int *distances = new int[num_distance];
-	int total_distance = 0;
-	for (int i = 0; i < num_distance; i++) {
-		cin >> distances[i];
-		total_distance += distances[i];
+	int num_exit, num_query;						// number of exits of the highway; number of quries
+	scanf("%d", &num_exit);
+	int *distances = new int[num_exit];				// distance between the first exit and each of the other exits
+	for (int i = 0; i < num_exit; i++) {
+		scanf("%d", &distances[i]);
 		if (i > 0)
-			distances[i] += distances[i - 1];
+			distances[i] += distances[i - 1];		// the last item is the length of the highway
 	}
-	cin >> num_query;
+	scanf("%d", &num_query);
 	for (int i = 0; i < num_query; i++) {
-		int head, tail;
-		cin >> head >> tail;
-		int distance = (tail > 1 ? distances[tail - 2] : 0) - (head > 1 ? distances[head - 2] : 0);
+		int a, b;
+		scanf("%d %d", &a, &b);
+		int distance = (a > 1 ? distances[a - 2] : 0) - (b > 1 ? distances[b - 2] : 0);
 		if (distance < 0)
 			distance = -distance;
-		cout << (distance > (total_distance >> 1) ? total_distance - distance : distance) << endl;
+		if (distance * 2 > distances[num_exit - 1])	// since the highway is a circle, the distance should be no larger than half of its length
+			distance = distances[num_exit - 1] - distance;
+		printf("%d\n", distance);
 	}
 
 	return 0;

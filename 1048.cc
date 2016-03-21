30b5612dea92b8bf51b85d00b04a107cc5cffd33
Mourzoufle <Mourzoufle@163.com>
2016-03-22 00:41:19 +0800

    1048 - final

--- 1048.cc
+++ 1048.cc
@@ -1,28 +1,28 @@
-#include <iostream>
+#include <cstdio>
 #include <algorithm>
 
 using namespace std;
 
 int main() {
 	int num_coin, amount;
-	cin >> num_coin >> amount;
+	scanf("%d %d", &num_coin, &amount);
 	int *coins = new int[num_coin];
 	for (int i = 0; i < num_coin; i++)
-		cin >> coins[i];
-	sort(coins, coins + num_coin);
-	int head = 0, tail = num_coin - 1;
-	while (head < tail) {
-		int sum = coins[head] + coins[tail];
-		if (sum == amount) {
-			cout << coins[head] << " " << coins[tail];
+		scanf("%d", &coins[i]);
+	sort(coins, coins + num_coin);		// sort the coins according to their face values
+
+	int head = 0, tail = num_coin - 1;	// start searching from coins with the smallest and the largest face values
+	while (head < tail) {				// when head >= tail, all the possible solutions are checked
+		int cur_amount = coins[head] + coins[tail];
+		if (cur_amount == amount) {		// found the solution with head having smallest value
+			printf("%d %d", coins[head], coins[tail]);
 			return 0;
 		}
-		else if (sum > amount)
+		else if (cur_amount > amount)	// current amount is too large, make tail smaller
 			tail--;
-		else
+		else							// or make the head larger
 			head++;
 	}
-	cout << "No Solution";
-
+	printf("No Solution");
 	return 0;
 }

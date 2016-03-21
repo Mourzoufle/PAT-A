8d5fe60b10518584f5b6294d731243876a61ca12
Mourzoufle <Mourzoufle@163.com>
2016-03-21 11:38:46 +0800

    1047 - final

--- 1047.cc
+++ 1047.cc
@@ -1,29 +1,30 @@
-#include <vector>
-#include <string>
-#include <algorithm>
 #include <cstdio>
+#include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
 	int num_student, num_course;
 	scanf("%d %d", &num_student, &num_course);
-	vector<string> *regs = new vector<string>[num_course];
+	vector<string> *students_regs = new vector<string>[num_course];	// registered students of each course
 	for (int i = 0; i < num_student; i++) {
-		char student[5];
+		char student[8];
 		int num_reg;
 		scanf("%s %d", student, &num_reg);
 		for (int j = 0; j < num_reg; j++) {
 			int course;
 			scanf("%d", &course);
-			regs[--course].push_back(student);
+			students_regs[--course].push_back(student);
 		}
 	}
+
 	for (int i = 0; i < num_course; i++) {
-		printf("%d %d\n", i + 1, regs[i].size());
-		sort(regs[i].begin(), regs[i].end());
-		for (vector<string>::iterator iter = regs[i].begin(); iter != regs[i].end(); iter++)
-			printf("%s\n", iter->c_str());
+		sort(students_regs[i].begin(), students_regs[i].end());
+		printf("%d %d\n", i + 1, students_regs[i].size());
+		for (int j = 0; j < students_regs[i].size(); j++)
+			printf("%s\n", students_regs[i][j].c_str());
 	}
 
 	return 0;

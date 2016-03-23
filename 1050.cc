0f97cb0b9a7a8d09a4726499276e91c9b6ed24fa
Mourzoufle <Mourzoufle@163.com>
2016-03-23 21:23:35 +0800

    1050 - final

--- 1050.cc
+++ 1050.cc
@@ -1,14 +1,16 @@
 #include <cstdio>
 
 int main() {
-	bool del[256];
-	char str[1 << 14], dels[1 << 14];
+	bool erase[256];					// flags of all the ASCII characters to be erased
+	for (int i = 0; i < 256; i++)
+		erase[i] = false;
+	char str[16384], str_erase[16384];	// the original string; the string in which all the characters should be erased
 	gets(str);
-	gets(dels);
-	for (int i = 0; dels[i] != '\0'; i++)
-		del[dels[i]] = true;
+	gets(str_erase);
+	for (int i = 0; str_erase[i] != '\0'; i++)
+		erase[str_erase[i]] = true;
 	for (int i = 0; str[i] != '\0'; i++)
-		if (!del[str[i]])
+		if (!erase[str[i]])
 			putchar(str[i]);
 
 	return 0;

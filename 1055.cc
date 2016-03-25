f32c8dac97926cbb2b60e6e3991c9ff28c4cc9bd
Mourzoufle <Mourzoufle@163.com>
2016-03-25 19:19:32 +0800

    1055 - final

--- 1055.cc
+++ 1055.cc
@@ -1,55 +1,58 @@
-#include <iostream>
+#include <cstdio>
+#include <cstring>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
+const int MAX_AGE = 200;
+const int MAX_DISPLAY = 100;								// maximum number of outputs
+
 struct Person {
-	string name;
+	char name[16];
 	int age;
-	int net_worth;
+	int worth;
 };
 
-bool compare(const Person &a, const Person &b) {
-	if (a.net_worth != b.net_worth)
-		return a.net_worth > b.net_worth;
-	else if (a.age != b.age)
+/* compare fuction for sorting people - according to the worth first, then the age, then the names */
+bool cmp(const Person &a, const Person &b) {
+	if (a.worth != b.worth)
+		return a.worth > b.worth;
+	if (a.age != b.age)
 		return a.age < b.age;
-	else
-		return a.name < b.name;
+	return strcmp(a.name, b.name) < 0;
 }
 
 int main() {
 	int num_person, num_query;
-	cin >> num_person >> num_query;
+	scanf("%d %d", &num_person, &num_query);
 	Person *people = new Person[num_person];
 	for (int i = 0; i < num_person; i++)
-		cin >> people[i].name >> people[i].age >> people[i].net_worth;
+		scanf("%s %d %d", people[i].name, &people[i].age, &people[i].worth);
+	sort(people, people + num_person, cmp);
 
-	sort(people, people + num_person, compare);
-	int age_counts[200];
-	for (int i = 0; i < 200; i++)
-		age_counts[i] = 0;
-	vector<Person *> list;
+	int counts_age[MAX_AGE];								// count of the people in each age
+	for (int i = 0; i < MAX_AGE; i++)
+		counts_age[i] = 0;
+	vector<Person *> people_valid;							// the people that are possible to be output
 	for (int i = 0; i < num_person; i++)
-		if (++age_counts[people[i].age - 1] <= 100)
-			list.push_back(people + i);
+		if (++counts_age[people[i].age - 1] <= MAX_DISPLAY)	// if the people at this age is more than the maximum number of outputs, the surplus people are ignored
+			people_valid.push_back(people + i);
 
 	for (int i = 0; i < num_query; i++) {
-		int max, lower, upper;
-		cin >> max >> lower >> upper;
-		cout << "Case #" << i + 1 << ":" << endl;
-		int num_output = 0;
-		for (int j = 0; j < list.size(); j++) {
-			if ((list[j]->age < lower) || (list[j]->age > upper))
+		int max_display, age_lower, age_upper;
+		scanf("%d %d %d", &max_display, &age_lower, &age_upper);
+		printf("Case #%d:\n", i + 1);
+		int num_display = 0;
+		for (int j = 0; j < people_valid.size(); j++) {
+			if ((people_valid[j]->age < age_lower) || (people_valid[j]->age > age_upper))
 				continue;
-			if (++num_output > max)
+			if (++num_display > max_display)				// the number of outputs has reached the upper bound
 				break;
-			else
-				cout << list[j]->name << " " << list[j]->age << " " << list[j]->net_worth << endl;
+			printf("%s %d %d\n", people_valid[j]->name, people_valid[j]->age, people_valid[j]->worth);
 		}
-		if (num_output == 0)
-			cout << "None" << endl;
+		if (num_display == 0)
+			printf("None\n");
 	}
 
 	return 0;

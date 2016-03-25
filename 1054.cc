2afb194a4698361c8a0322a42795fce784e368a7
Mourzoufle <Mourzoufle@163.com>
2016-03-25 18:49:28 +0800

    1054 - final

--- 1054.cc
+++ 1054.cc
@@ -1,25 +1,24 @@
-#include <iostream>
+#include <cstdio>
 #include <map>
 
 using namespace std;
 
 int main() {
-	int col, row;
-	cin >> col >> row;
-	int mid = (col * row + 1) >> 1;
-	map<int, int> hist;
+	int row, col;
+	scanf("%d %d", &row, &col);
+	int threshold = (col * row + 1) / 2;		// the dominant color must occur in at least half of the total pixels
+	map<int, int> counts;						// count of each color
 	for (int i = 0; i < row; i++) {
 		for (int j = 0; j < col; j++) {
-			int pixel;
-			cin >> pixel;
-			if (hist.find(pixel) == hist.end())
-				hist[pixel] = 0;
-			if (++hist[pixel] >= mid) {
-				cout << pixel;
+			int color;
+			scanf("%d", &color);
+			if (counts.find(color) == counts.end())
+				counts[color] = 0;
+			if (++counts[color] >= threshold) {	// dominant color found
+				printf("%d", color);
 				return 0;
 			}
 		}
 	}
-
 	return 0;
 }

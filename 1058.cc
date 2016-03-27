fa24eb2bd68f72742b21df486f8fc268747a0a8f
Mourzoufle <Mourzoufle@163.com>
2016-03-27 17:08:56 +0800

    1058 - final

--- 1058.cc
+++ 1058.cc
@@ -1,11 +1,19 @@
 #include <cstdio>
 
 int main() {
-	long long galleon_a, sickle_a, knut_a;
-	long long galleon_b, sickle_b, knut_b;
-	scanf("%lld.%lld.%lld", &galleon_a, &sickle_a, &knut_a);
-	scanf("%lld.%lld.%lld", &galleon_b, &sickle_b, &knut_b);
-	printf("%lld.%lld.%lld", galleon_a + galleon_b + (sickle_a + sickle_b + (knut_a + knut_b > 28 ? 1 : 0) > 16 ? 1 : 0), (sickle_a + sickle_b + (knut_a + knut_b > 28 ? 1 : 0)) % 17, (knut_a + knut_b) % 29);
+	int galleon_a, sickle_a, knut_a, galleon_b, sickle_b, knut_b;
+	scanf("%d.%d.%d %d.%d.%d", &galleon_a, &sickle_a, &knut_a, &galleon_b, &sickle_b, &knut_b);
 
+	int galleon = galleon_a + galleon_b, sickle = sickle_a + sickle_b, knut = knut_a + knut_b;	// the sum in each kind of currencies
+	if (knut > 28) {																			// has carry
+		sickle++;
+		knut %= 29;
+	}
+	if (sickle > 16) {																			// has carry
+		galleon++;
+		sickle %= 17;
+	}
+	printf("%d.%d.%d", galleon, sickle, knut);
+	
 	return 0;
 }

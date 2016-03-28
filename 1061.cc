2d47d41e7a6749578b245d5e04994d52bf811dae
Mourzoufle <Mourzoufle@163.com>
2016-03-28 22:06:10 +0800

    1061 - final

--- 1061.cc
+++ 1061.cc
@@ -1,42 +1,37 @@
-#include <iostream>
 #include <cstdio>
 
-using namespace std;
-
-string DAYS[] = {
-	"MON ",
-	"TUE ",
-	"WED ",
-	"THU ",
-	"FRI ",
-	"SAT ",
-	"SUN "
+const char DAYS[][4] = {
+	"MON",
+	"TUE",
+	"WED",
+	"THU",
+	"FRI",
+	"SAT",
+	"SUN"
 };
 
 int main() {
-	string codes[4];
+	char strs[4][64];																														// the strings
 	for (int i = 0; i < 4; i++)
-		cin >> codes[i];
-	int idx = 0;
-	while ((codes[0][idx] < 'A') || (codes[0][idx] > 'G') || (codes[0][idx] != codes[1][idx]))
+		scanf("%s", strs[i]);
+
+	int idx = 0;																															// the index of current char
+	while ((strs[0][idx] < 'A') || (strs[0][idx] > 'G') || (strs[0][idx] != strs[1][idx]))													// find a common capital letter in the first two strings
 		idx++;
-	cout << DAYS[codes[0][idx] - 'A'];
-	while (true) {
-		if (codes[0][++idx] != codes[1][idx])
-			continue;
-		else if ((codes[0][idx] >= 'A') && (codes[0][idx] <= 'N')) {
-			printf("%d:", codes[0][idx] - 'A' + 10);
-			break;
-		}
-		else if ((codes[0][idx] >= '0') && (codes[0][idx] <= '9')) {
-			printf("0%d:", codes[0][idx] - '0');
+	printf("%s ", DAYS[strs[0][idx] - 'A']);
+	while (++idx) {
+		if (strs[0][idx] == strs[1][idx]) {																									// find next common character in the first two strings
+			if ((strs[0][idx] >= 'A') && (strs[0][idx] <= 'N'))
+				printf("%d:", strs[0][idx] - 'A' + 10);
+			else if ((strs[0][idx] >= '0') && (strs[0][idx] <= '9'))
+				printf("0%d:", strs[0][idx] - '0');
+			else																															// not indicates the hour, continue to find
+				continue;
 			break;
 		}
 	}
-	for (int i = 0; i < codes[2].length(); i++) {
-		if (codes[2][i] != codes[3][i])
-			continue;
-		else if (((codes[2][i] >= 'A') && (codes[2][i] <= 'Z')) || ((codes[2][i] >= 'a') && (codes[2][i] <= 'z'))) {
+	for (int i = 0; strs[2][i] != '\0'; i++) {
+		if ((strs[2][i] == strs[3][i]) && (((strs[2][i] >= 'A') && (strs[2][i] <= 'Z')) || ((strs[2][i] >= 'a') && (strs[2][i] <= 'z')))) {	// find a common English letter in the last two strings
 			printf("%02d", i);
 			break;
 		}

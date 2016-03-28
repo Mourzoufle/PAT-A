447ddc1c790db9ce1503e07bcdf9ade55222018f
Mourzoufle <Mourzoufle@163.com>
2016-03-28 22:19:10 +0800

    1062 - final

--- 1062.cc
+++ 1062.cc
@@ -10,7 +10,8 @@ struct Person {
 	int talent;
 };
 
-bool compare(const Person *a, const Person *b) {
+/* compare fuction for sorting records - sorted by score, then id if the scores are equal */
+bool cmp(const Person *a, const Person *b) {
 	if (a->virtue + a->talent != b->virtue + b->talent)
 		return a->virtue + a->talent > b->virtue + b->talent;
 	if (a->virtue != b->virtue)
@@ -20,29 +21,28 @@ bool compare(const Person *a, const Person *b) {
 }
 
 int main() {
-	vector<Person *> lists[4];
-	int num_person, lower, higher;
-	scanf("%d %d %d", &num_person, &lower, &higher);
+	vector<Person *> people_valids[4];									// people that are qualified as sages, noblemen, fools, and small men
+	int num_person, lower, upper;										// number of people; the lower bound of the qualified grades; the higher line of qualification
+	scanf("%d %d %d", &num_person, &lower, &upper);
 	Person *people = new Person[num_person];
 	for (int i = 0; i < num_person; i++) {
 		scanf("%d %d %d", &people[i].id, &people[i].virtue, &people[i].talent);
-		if ((people[i].virtue < lower) || (people[i].talent < lower))
+		if ((people[i].virtue < lower) || (people[i].talent < lower))	// not qualified to be ranked
 			continue;
-		if ((people[i].virtue >= higher) && (people[i].talent >= higher))
-			lists[0].push_back(people + i);
-		else if (people[i].virtue >= higher)
-			lists[1].push_back(people + i);
-		else if (people[i].virtue >= people[i].talent)
-			lists[2].push_back(people + i);
-		else
-			lists[3].push_back(people + i);
+		if ((people[i].virtue >= upper) && (people[i].talent >= upper))	// sage
+			people_valids[0].push_back(people + i);
+		else if (people[i].virtue >= upper)								// nobleman
+			people_valids[1].push_back(people + i);
+		else if (people[i].virtue >= people[i].talent)					// fool
+			people_valids[2].push_back(people + i);
+		else															// small man
+			people_valids[3].push_back(people + i);
 	}
-
-	printf("%d\n", lists[0].size() + lists[1].size() + lists[2].size() + lists[3].size());
+	printf("%d\n", people_valids[0].size() + people_valids[1].size() + people_valids[2].size() + people_valids[3].size());
 	for (int i = 0; i < 4; i++) {
-		sort(lists[i].begin(), lists[i].end(), compare);
-		for (vector<Person *>::iterator iter = lists[i].begin(); iter != lists[i].end(); iter++)	
-			printf("%08d %d %d\n", (*iter)->id, (*iter)->virtue, (*iter)->talent);
+		sort(people_valids[i].begin(), people_valids[i].end(), cmp);
+		for (int j = 0; j < people_valids[i].size(); j++)	
+			printf("%08d %d %d\n", people_valids[i][j]->id, people_valids[i][j]->virtue, people_valids[i][j]->talent);
 	}
 
 	return 0;

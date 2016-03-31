1beac6bbea31494a262496d6d0dec7f77de9d1c9
Mourzoufle <Mourzoufle@163.com>
2016-04-01 03:07:56 +0800

    1069 - final

--- 1069.cc
+++ 1069.cc
@@ -3,32 +3,29 @@
 
 using namespace std;
 
-int ordered(int num, int is_inc) {
-	int *digits = new int[4];
+/* compute the value of the number with each digit sorted in increasing (inc = 1) or decreasing (inc = -1) order */
+int get_ordered(int num, int inc) {
+	int digits[4];
 	for (int i = 0; i < 4; i++) {
 		digits[i] = num % 10;
 		num /= 10;
 	}
 	sort(digits, digits + 4);
-	int res = 0;
-	if (is_inc)
-		for (int i = 0; i < 4; i++)
-			res = res * 10 + digits[i];
-	else
-		for (int i = 3; i >= 0; i--)
-			res = res * 10 + digits[i];
+	int value = 0;
+	for (int i = (inc > 0 ? 0 : 3); (i < 4) && (i >= 0); i += inc)
+		value = value * 10 + digits[i];
 
-	return res;
+	return value;
 }
 
 int main() {
 	int num;
 	scanf("%d", &num);
 	while (true) {
-		int inc = ordered(num, true), dec = ordered(num, false);
+		int inc = get_ordered(num, 1), dec = get_ordered(num, -1);	// value of the number with each digit sorted in increasing and decreasing order
 		num = dec - inc;
 		printf("%04d - %04d = %04d\n", dec, inc, num);
-		if ((num == 0) || (num == 6174))
+		if ((num == 0) || (num == 6174))							// halt when 6174 comes or all the digits are the same
 			break;
 	}
 

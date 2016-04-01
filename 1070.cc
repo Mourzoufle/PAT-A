6e67ffc6d8432ec7092cb20bb4f5abd92d38c2e1
Mourzoufle <Mourzoufle@163.com>
2016-04-01 17:18:30 +0800

    1070 - final

--- 1070.cc
+++ 1070.cc
@@ -4,32 +4,30 @@
 
 using namespace std;
 
-struct Item {
+struct Item {												// the kind of mooncakes
 	double amount;
 	double price;
 };
 
-bool compare(const Item &a, const Item &b) { return a.price / a.amount > b.price / b.amount; };
+/* cmp fuction for sorting kinds of mooncakes - according to their price per ton */
+bool cmp(const Item &a, const Item &b) { return a.price / a.amount > b.price / b.amount; };
 
 int main() {
-	int num_item;
-	double amount;
-	cin >> num_item >> amount;
+	int num_item, amount;
+	scanf("%d %d", &num_item, &amount);
 	Item *items = new Item[num_item];
 	for (int i = 0; i < num_item; i++)
-		cin >> items[i].amount;
+		scanf("%lf", &items[i].amount);
 	for (int i = 0; i < num_item; i++)
-		cin >> items[i].price;
-	sort(items, items + num_item, compare);
+		scanf("%lf", &items[i].price);
+	sort(items, items + num_item, cmp);
 
 	double profit = 0;
-	for (int i = 0; i < num_item; i++) {
-		if (amount <= 0)
-			break;
-		profit += amount < items[i].amount ? items[i].price * amount / items[i].amount : items[i].price;
+	for (int i = 0; (i < num_item) && (amount > 0); i++) {	// halt when the required amount is met
+		profit += min(items[i].price * amount / items[i].amount, items[i].price);
 		amount -= items[i].amount;
 	}
-	printf("%.2f", profit);
+	printf("%.2lf", profit);
 
 	return 0;
 }

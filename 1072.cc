5a236164d43938e443c07d4605089d68c68d82cf
Mourzoufle <Mourzoufle@163.com>
2016-04-01 23:49:26 +0800

    1072 - final

--- 1072.cc
+++ 1072.cc
@@ -1,48 +1,38 @@
-#include <iostream>
-#include <string>
-#include <sstream>
-#include <algorithm>
 #include <cstdio>
+#include <cstdlib>
 #include <climits>
 #include <cfloat>
 
 using namespace std;
 
-int str_to_idx(string str, int num) {
-	if (str[0] == 'G')
-		str = str.substr(1);
-	else
-		num = 0;
-	stringstream ss;
-	int idx;
-	ss << str;
-	ss >> idx;
-
-	return idx + num - 1;
+/* convert string representation of houses and locations for gas station to indices in the map */
+int str_to_idx(char *str, int num_house) {
+	if (str[0] == 'G')														// a location for gas station
+		return atoi(str + 1) + num_house - 1;
+	return atoi(str) - 1;													// a house
 }
 
-int* Dijstra(int **roads, int num_node, int dst) {
+/* Dijstra - find the shortest distances of every node to the destination */
+int* dijstra(int **roads, int num_node, int dst) {
 	int *distances = new int[num_node];
 	bool *visit = new bool[num_node];
 	for (int i = 0; i < num_node; i++) {
-		distances[i] = roads[i][dst];
-		visit[i] = false;
+		distances[i] = roads[i][dst];										// initial distance of each node - the direct connection to the destination
+		visit[i] = false;													// every node is unvisited
 	}
-	visit[dst] = true;
-	for (int i = 1; i < num_node; i++) {
-		int idx = -1;
-		for (int j = 0; j < num_node; j++) {
-			if (visit[j])
-				continue;
-			if ((idx < 0) || (distances[idx] > distances[j]))
-				idx = j;
-		}
-		if (distances[idx] == INT_MAX)
-			break;
-		visit[idx] = true;
+	visit[dst] = true;														// ...except for the destination
+
+	for (int i = 1; i < num_node; i++) {									// the destination is already visited
+		int cur_node = -1;													// current node that is unvisited & has the shortest distance
 		for (int j = 0; j < num_node; j++)
-			if (distances[j] - distances[idx] > roads[j][idx])
-				distances[j] = roads[j][idx] + distances[idx];
+			if (!visit[j] && ((cur_node < 0) || (distances[j] < distances[cur_node])))
+				cur_node = j;
+		if (distances[cur_node] == INT_MAX)									// all unvisited nodes are not connected to the destination - exit
+			break;
+		visit[cur_node] = true;
+		for (int j = 0; j < num_node; j++)
+			if (roads[j][cur_node] < distances[j] - distances[cur_node])	// this node has a shorter distance via the current node
+				distances[j] = roads[j][cur_node] + distances[cur_node];
 	}
 	delete[] visit;
 
@@ -50,31 +40,32 @@ int* Dijstra(int **roads, int num_node, int dst) {
 }
 
 int main() {
-	int num_house, num_station, num_road, max_distance;
-	cin >> num_house >> num_station >> num_road >> max_distance;
-	num_station += num_house;
-	int **roads = new int*[num_station];
-	for (int i = 0; i < num_station; i++) {
-		roads[i] = new int[num_station];
-		for (int j = 0; j < num_station; j++)
+	int num_house, num_location, num_road, max_distance;
+	scanf("%d %d %d %d", &num_house, &num_location, &num_road, &max_distance);
+	int num_node = num_house + num_location;								// number of nodes on the map
+	int **roads = new int*[num_node];
+	for (int i = 0; i < num_node; i++) {
+		roads[i] = new int[num_node];
+		for (int j = 0; j < num_node; j++)
 			roads[i][j] = INT_MAX;
 		roads[i][i] = 0;
 	}
 	for (int i = 0; i < num_road; i++) {
-		string str_a, str_b;
+		char a[8], b[8];													// the ends of each road
 		int distance;
-		cin >> str_a >> str_b >> distance;
-		int idx_a = str_to_idx(str_a, num_house), idx_b = str_to_idx(str_b, num_house);
+		scanf("%s %s %d", a, b, &distance);
+		int idx_a = str_to_idx(a, num_house), idx_b = str_to_idx(b, num_house);
 		roads[idx_a][idx_b] = distance;
 		roads[idx_b][idx_a] = distance;
 	}
-	int idx = -1;
+
+	int location;
 	double min = 0, avg = DBL_MAX;
-	for (int i = num_house; i < num_station; i++) {
-		int *distances = Dijstra(roads, num_station, i);
-		double cur_min = max_distance, cur_avg = 0;
+	for (int i = num_house; i < num_node; i++) {
+		int *distances = dijstra(roads, num_node, i);						// calculate the shortest distances of the houses to each location
+		double cur_min = DBL_MAX, cur_avg = 0;
 		for (int j = 0; j < num_house; j++) {
-			if (distances[j] > max_distance) {
+			if (distances[j] > max_distance) {								// one house is out of the max service range, do not choose this location to build gas station
 				cur_min = -1;
 				break;
 			}
@@ -82,17 +73,17 @@ int main() {
 				cur_min = distances[j];
 			cur_avg += distances[j];
 		}
-		delete[] distances;
-		if ((min < cur_min) || ((min == cur_min) && (avg > cur_avg))) {
-			idx = i;
+		if ((cur_min > min) || ((cur_min == min) && (avg > cur_avg))) {		// found a better location
+			location = i;
 			min = cur_min;
 			avg = cur_avg;
 		}
+		delete[] distances;
 	}
-	if (idx < 0)
-		cout << "No Solution";
+	if (avg == DBL_MAX)														// no solution has ever found
+		printf("No Solution");
 	else
-		printf("G%d\n%.1f %.1f", ++idx - num_house, min, avg / num_house);
+		printf("G%d\n%.1lf %.1lf", ++location - num_house, min, avg / num_house);
 
 	return 0;
 }

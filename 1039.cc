cb201e8b4b7d3dd1dfbe92bdaf6d199c4b062771
Mourzoufle <Mourzoufle@163.com>
2016-04-01 22:36:16 +0800

    1039 - final

--- 1039.cc
+++ 1039.cc
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-const int MAX_STUDENT = 26 * 26 * 26 * 10;							// max number of students (since each of them is represented by three capital letters and one digit)
+const int MAX_STUDENT = 26 * 26 * 26 * 10;									// max number of students (since each of them is represented by three capital letters and one digit)
 
 /* map each string of name to an integer */
 int str_to_num(char *str) { return (str[0] - 'A') * 6760 + (str[1] - 'A') * 260 + (str[2] - 'A') * 10 + str[3] - '0'; }
@@ -12,27 +12,27 @@ int str_to_num(char *str) { return (str[0] - 'A') * 6760 + (str[1] - 'A') * 260 
 int main() {
 	int num_student, num_course;
 	scanf("%d %d", &num_student, &num_course);
-	vector<int> *courses_regs = new vector<int>[MAX_STUDENT];		// list of courses each student registers
+	vector<int> *courses_students = new vector<int>[MAX_STUDENT];			// courses each student registers
 	for (int i = 0; i < num_course; i++) {
 		int course, num_reg;
 		scanf("%d %d", &course, &num_reg);
 		for (int j = 0; j < num_reg; j++) {
 			char student[8];
 			scanf("%s", student);
-			courses_regs[str_to_num(student)].push_back(course);
+			courses_students[str_to_num(student)].push_back(course);
 		}
 	}
 
 	for (int i = 0; i < MAX_STUDENT; i++)
-		if (!courses_regs[i].empty())
-			sort(courses_regs[i].begin(), courses_regs[i].end());	// the coursed will be listed according to their indices
+		if (!courses_students[i].empty())
+			sort(courses_students[i].begin(), courses_students[i].end());	// the coursed will be listed according to their indices
 	for (int i = 0; i < num_student; i++) {
 		char student[8];
 		scanf("%s", student);
 		int idx_student = str_to_num(student);
-		printf("%s %d", student, courses_regs[idx_student].size());
-		for (int j = 0; j < courses_regs[idx_student].size(); j++)
-			printf(" %d", courses_regs[idx_student][j]);
+		printf("%s %d", student, courses_students[idx_student].size());
+		for (int j = 0; j < courses_students[idx_student].size(); j++)
+			printf(" %d", courses_students[idx_student][j]);
 		printf("\n");
 	}
 

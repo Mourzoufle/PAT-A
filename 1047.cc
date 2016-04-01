03d98e5ddc378f2916808bf1aad2fab41c68d659
Mourzoufle <Mourzoufle@163.com>
2016-04-01 22:37:49 +0800

    1047 - final

--- 1047.cc
+++ 1047.cc
@@ -8,7 +8,7 @@ using namespace std;
 int main() {
 	int num_student, num_course;
 	scanf("%d %d", &num_student, &num_course);
-	vector<string> *students_regs = new vector<string>[num_course];	// registered students of each course
+	vector<string> *students_courses = new vector<string>[num_course];	// registered students of each course
 	for (int i = 0; i < num_student; i++) {
 		char student[8];
 		int num_reg;
@@ -16,15 +16,15 @@ int main() {
 		for (int j = 0; j < num_reg; j++) {
 			int course;
 			scanf("%d", &course);
-			students_regs[--course].push_back(student);
+			students_courses[--course].push_back(student);
 		}
 	}
 
 	for (int i = 0; i < num_course; i++) {
-		sort(students_regs[i].begin(), students_regs[i].end());
-		printf("%d %d\n", i + 1, students_regs[i].size());
-		for (int j = 0; j < students_regs[i].size(); j++)
-			printf("%s\n", students_regs[i][j].c_str());
+		sort(students_courses[i].begin(), students_courses[i].end());
+		printf("%d %d\n", i + 1, students_courses[i].size());
+		for (int j = 0; j < students_courses[i].size(); j++)
+			printf("%s\n", students_courses[i][j].c_str());
 	}
 
 	return 0;

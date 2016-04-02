746c731ffca8974201ee955c6093f7096e093a75
Mourzoufle <Mourzoufle@163.com>
2016-04-03 01:27:44 +0800

    1076 - final

--- 1076.cc
+++ 1076.cc
@@ -1,59 +1,63 @@
-#include <iostream>
-#include <list>
+#include <cstdio>
+#include <vector>
 #include <queue>
 
 using namespace std;
 
-int BFS(int *reposts, list<int> *users, int num_user, int depth, int user) {
-	bool *post = new bool[num_user];
+/* BFS - compute number of forwards the given user can get */
+int get_num_forward(vector<int> *followers_users, int num_user, int user, int level) {
+	bool *forward = new bool[num_user];							// flags indicating whether each user has forwarded the post
 	for (int i = 0; i < num_user; i++)
-		post[i] = false;
-	post[user] = true;
-	queue<int> followers;
-	followers.push(user);
-	for (int i = 0; i < depth; i++) {
-		int num_follower = followers.size();
-		for (int j = 0; j < num_follower; j++) {
-			int follower = followers.front();
-			followers.pop();
-			for (list<int>::iterator iter = users[follower].begin(); iter != users[follower].end(); iter++) {
-				if (!post[*iter]) {
-					followers.push(*iter);
-					post[*iter] = true;
+		forward[i] = false;
+	forward[user] = true;
+	int num_forward = 0;
+	queue<int> queue;											// BFS
+	queue.push(user);
+	for (int i = 0; i < level; i++) {
+		int cur_size = queue.size();
+		for (int j = 0; j < cur_size; j++) {
+			int user = queue.front();
+			for (int k = 0; k < followers_users[user].size(); k++) {
+				int follower = followers_users[user][k];
+				if (!forward[follower]) {						// this follower has not forwarded the post yet
+					queue.push(follower);
+					num_forward++;
+					forward[follower] = true;
 				}
 			}
+			queue.pop();
 		}
 	}
-	for (int i = 0; i < num_user; i++)
-		if (post[i])
-			reposts[user]++;
-	delete[] post;
+	delete[] forward;
 
-	return reposts[user];
+	return num_forward;
 }
 
 int main() {
-	int num_user, depth;
-	cin >> num_user >> depth;
-	list<int> *users = new list<int>[num_user];
+	int num_user, level;										// number of users; number of levels of indirect followers that are counted
+	scanf("%d %d", &num_user, &level);
+	vector<int> *followers_users = new vector<int>[num_user];	// followers of each user
 	for (int i = 0; i < num_user; i++) {
-		int num_follow;
-		cin >> num_follow;
+		int num_follow;											// number of users this user follows
+		scanf("%d", &num_follow);
 		for (int j = 0; j < num_follow; j++) {
 			int user;
-			cin >> user;
-			users[--user].push_back(i);
+			scanf("%d", &user);
+			followers_users[--user].push_back(i);
 		}
 	}
-	int *reposts = new int[num_user];
+
+	int *nums_forward = new int[num_user];						// number of forwards each user can get
 	for (int i = 0; i < num_user; i++)
-		reposts[i] = -1;
+		nums_forward[i] = -1;									// all the numbers of forwards are not computed yet
 	int num_query;
-	cin >> num_query;
-	for (int r = 0; r < num_query; r++) {
+	scanf("%d", &num_query);
+	for (int i = 0; i < num_query; i++) {
 		int user;
-		cin >> user;
-		cout << (reposts[user - 1] < 0 ? BFS(reposts, users, num_user, depth, user - 1) : reposts[user - 1]) << endl;
+		scanf("%d", &user);
+		if (nums_forward[--user] < 0)
+			nums_forward[user] = get_num_forward(followers_users, num_user, user, level);
+		printf("%d\n", nums_forward[user]);
 	}
 
 	return 0;

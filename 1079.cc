9cafc43792c778d5297dc59beef431a77fa58dd9
Mourzoufle <Mourzoufle@163.com>
2016-04-03 13:25:24 +0800

    1079 - final

--- 1079.cc
+++ 1079.cc
@@ -1,41 +1,42 @@
 #include <cstdio>
-#include <list>
+#include <vector>
 
 using namespace std;
 
 struct Node {
-	list<int> children;
+	vector<int> children;
 	double amount;
 };
 
-double DFS(Node *nodes, Node *root, double price, double mul) {
-	if (root->children.empty())
-		return root->amount * price;
+/* DFS - get the total amount of the sales */
+double get_amount(Node *nodes, int root, double base_price, double mul) {
+	if (nodes[root].children.empty())	// this node is a retailer
+		return nodes[root].amount * base_price;
 	double sum = 0;
-	for (list<int>::iterator iter = root->children.begin(); iter != root->children.end(); iter++)
-		sum += DFS(nodes, nodes + *iter, price * mul, mul);
+	for (int i = 0; i < nodes[root].children.size(); i++)
+		sum += get_amount(nodes, nodes[root].children[i], base_price * mul, mul);
 	return sum;
 }
 
 int main() {
 	int num_node;
-	double price, percent;
-	scanf("%d %lf %lf", &num_node, &price, &percent);
+	double base_price, ratio;			// base price; percentage rate of price increment for each distributor or retailer
+	scanf("%d %lf %lf", &num_node, &base_price, &ratio);
 	Node *nodes = new Node[num_node];
 	for (int i = 0; i < num_node; i++) {
 		int num_child;
 		scanf("%d", &num_child);
-		if (num_child > 0) {
+		if (num_child > 0) {			// this node is a supplier or distributor
 			for (int j = 0; j < num_child; j++) {
 				int child;
 				scanf("%d", &child);
 				nodes[i].children.push_back(child);
 			}
 		}
-		else
+		else							// this node is a retailer
 			scanf("%lf", &nodes[i].amount);
 	}
-	printf("%.1f", DFS(nodes, nodes, price, 1 + percent / 100));
+	printf("%.1lf", get_amount(nodes, 0, base_price, 1 + ratio / 100));
 
 	return 0;
 }

06cdec9b39ffa194b5e0c04dce4c9073f25669e5
Mourzoufle <Mourzoufle@163.com>
2016-04-04 14:56:11 +0800

    1083 - final

--- 1083.cc
+++ 1083.cc
@@ -1,34 +1,37 @@
-#include <iostream>
+#include <cstdio>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
 
 struct Record {
-	string name;
-	string id;
+	char name[16];
+	char id[16];
 	int grade;
 };
 
-bool compare(const Record &a, const Record &b) { return a.grade > b.grade; }
+/* compare fuction for sorting records - according to grades */
+bool cmp(Record *a, Record *b) { return a->grade > b->grade; }
 
 int main() {
 	int num_record;
-	cin >> num_record;
+	scanf("%d", &num_record);
 	Record *records = new Record[num_record];
 	for (int i = 0; i < num_record; i++)
-		cin >> records[i].name >> records[i].id >> records[i].grade;
-	sort(records, records + num_record, compare);
-	int lower, upper;
-	cin >> lower >> upper;
-	if ((records[0].grade < lower) || (records[num_record - 1].grade > upper))
-		cout << "NONE";
+		scanf("%s %s %d", records[i].name, records[i].id, &records[i].grade);
+	int lower, upper;	// lower and upper bounds of the interval
+	scanf("%d %d", &lower, &upper);
+
+	vector<Record *> records_valid;
+	for (int i = 0; i < num_record; i++)
+		if ((records[i].grade >= lower) && (records[i].grade <= upper))
+			records_valid.push_back(records + i);
+	if (records_valid.empty())
+		printf("NONE");
 	else {
-		for (int i = 0; i < num_record; i++) {
-			if (records[i].grade < lower)
-				break;
-			else if (records[i].grade <= upper)
-				cout << records[i].name << " " << records[i].id << endl;
-		}
+		sort(records_valid.begin(), records_valid.end(), cmp);
+		for (int i = 0; i < records_valid.size(); i++)
+			printf("%s %s\n", records_valid[i]->name, records_valid[i]->id);
 	}
 
 	return 0;

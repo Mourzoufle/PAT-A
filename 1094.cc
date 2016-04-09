2f5381bd252c01bdc590294a544d0f8dd032b85c
Mourzoufle <Mourzoufle@163.com>
2016-04-09 15:35:06 +0800

    1094 - final

--- 1094.cc
+++ 1094.cc
@@ -1,42 +1,40 @@
-#include <iostream>
+#include <cstdio>
 #include <vector>
 #include <queue>
 
 using namespace std;
 
 int main() {
-	int num_node, num_line;
-	cin >> num_node >> num_line;
-	vector<int> *nodes = new vector<int>[num_node];
-	for (int i = 0; i < num_line; i++) {
+	int num_node, num_nonleaf;
+	scanf("%d %d", &num_node, &num_nonleaf);
+	vector<int> *nodes = new vector<int>[num_node];	// children of each node
+	for (int i = 0; i < num_nonleaf; i++) {
 		int node, num_child;
-		cin >> node >> num_child;
-		node--;
+		scanf("%d %d", &node, &num_child);
 		for (int j = 0; j < num_child; j++) {
 			int child;
-			cin >> child;
-			nodes[node].push_back(--child);
+			scanf("%d", &child);
+			nodes[node - 1].push_back(child - 1);
 		}
 	}
 
-	int max_size = 1, max_level = 1, level = 0;
-	queue<int> queue;
+	int max_size = 1, level = 1;					// the largest population number; level of the corresponding generation
+	queue<int> queue;								// level order traversal
 	queue.push(0);
-	while (!queue.empty()) {
-		int size = queue.size();
-		level++;
-		if (size > max_size) {
-			max_size = size;
-			max_level = level;
+	for (int i = 1; !queue.empty(); i++) {
+		int cur_size = queue.size();
+		if (cur_size > max_size) {					// larger generation found
+			max_size = cur_size;
+			level = i;
 		}
-		for (int i = 0; i < size; i++) {
-			int cur = queue.front();
+		for (int i = 0; i < cur_size; i++) {
+			int node = queue.front();
+			for (int j = 0; j < nodes[node].size(); j++)
+				queue.push(nodes[node][j]);
 			queue.pop();
-			for (int j = 0; j < nodes[cur].size(); j++)
-				queue.push(nodes[cur][j]);
 		}
 	}
-	cout << max_size << " " << max_level;
+	printf("%d %d", max_size, level);
 
 	return 0;
 }

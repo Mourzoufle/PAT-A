17ff62bdbe604569de7d0ce76a3ed26aa9444c1c
Mourzoufle <Mourzoufle@163.com>
2016-04-09 15:23:33 +0800

    1093 - final

--- 1093.cc
+++ 1093.cc
@@ -1,25 +1,19 @@
-#include <iostream>
-
-using namespace std;
+#include <cstdio>
 
 int main() {
-	string str;
-	cin >> str;
-	long long **counts = new long long*[str.size()];
-	for (int i = 0; i < str.size(); i++) {
-		counts[i] = new long long[3];
-		for (int j = 0; j < 3; j++)
-			counts[i][j] = 0;
+	char *str = new char[16384];
+	scanf("%s", str);
+
+	long long counts[3] = {0, 0, 0};	// number of "P", "PA", and "PAT" in the string
+	for (int i = 0; str[i] != '\0'; i++) {
+		if (str[i] == 'P')
+			counts[0]++;
+		else if (str[i] == 'A')
+			counts[1] += counts[0];
+		else
+			counts[2] += counts[1];
 	}
-	counts[0][0] = str[0] == 'P' ? 1 : 0;
-	counts[0][1] = 0;
-	counts[0][2] = 0;
-	for (int i = 1; i < str.size(); i++) {
-		counts[i][0] = counts[i - 1][0] + (str[i] == 'P' ? 1 : 0);
-		counts[i][1] = counts[i - 1][1] + (str[i] == 'A' ? counts[i - 1][0] : 0);
-		counts[i][2] = counts[i - 1][2] + (str[i] == 'T' ? counts[i - 1][1] : 0);
-	}
-	cout << counts[str.size() - 1][2] % 1000000007;
+	printf("%lld", counts[2] % 1000000007);
 
 	return 0;
 }

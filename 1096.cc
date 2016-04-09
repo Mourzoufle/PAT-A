535fd996f4f3c930f9e6a557a79a2dbe927af319
Mourzoufle <Mourzoufle@163.com>
2016-04-10 02:17:28 +0800

    1096 - final

--- 1096.cc
+++ 1096.cc
@@ -1,30 +1,29 @@
 #include <cstdio>
 #include <cmath>
 
-using namespace std;
-
 int main() {
 	int num;
 	scanf("%d", &num);
-	int start, length = 0;
-	int max = sqrt(num);
-	for (int i = 2; i <= max; i++) {
+
+	int head, max_length = 0;	// head of the consecutive factors; max length of the consecutive factors
+	int max_factor = sqrt(num);	// only need to check to this (possible) factor
+	for (int i = 2; i <= max_factor; i++) {
 		int cur_num = num, cur_length = 0;
 		for (int j = i; cur_num % j == 0; j++) {
 			cur_num /= j;
 			cur_length++;
 		}
-		if (length < cur_length) {
-			start = i;
-			length = cur_length;
+		if (cur_length > max_length) {
+			max_length = cur_length;
+			head = i;
 		}
 	}
-	if (length == 0)
+	if (max_length == 0)
 		printf("1\n%d", num);
 	else {
-		printf("%d\n%d", length, start);
-		for (int i = 1; i < length; i++)
-			printf("*%d", start + i);
+		printf("%d\n%d", max_length, head);
+		for (int i = 1; i < max_length; i++)
+			printf("*%d", head + i);
 	}
 
 	return 0;

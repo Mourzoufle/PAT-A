8714e1a732f283a648e4e403bfa626884be6f674
Mourzoufle <Mourzoufle@163.com>
2016-04-10 14:28:25 +0800

    1098 - final

--- 1098.cc
+++ 1098.cc
@@ -1,59 +1,46 @@
-#include <iostream>
+#include <cstdio>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
-	int num;
-	cin >> num;
-	int *ori = new int[num];
-	int *cur = new int[num];
-	for (int i = 0; i < num; i++)
-		cin >> ori[i];
-	for (int i = 0; i < num; i++)
-		cin >> cur[i];
+	int num_element;										// number of elements in the sequence
+	scanf("%d", &num_element);
+	int *elements = new int[num_element];					// the elements sequence
+	for (int i = 0; i < 2; i++)								// NOTE: the initial sequence is useless
+		for (int j = 0; j < num_element; j++)
+			scanf("%d", &elements[j]);
 
-	if (cur[0] < cur[1]) {
-		cout << "Insertion Sort" << endl;
-		int idx = 1;
-		while (++idx < num)
-			if (cur[idx] < cur[idx - 1])
-				break;
-		for (int i = 0; i < idx; i++) {
-			if (i > 0)
-				cout << " ";
-			if (cur[i] < cur[idx])
-				cout << cur[i];
-			else {
-				cout << cur[idx];
-				while (i < idx)
-					cout << " " << cur[i++];
+	if (elements[1] < elements[0]) {						// must be heap sort
+		printf("Heap Sort\n");
+		int size_heap = num_element;						// index of the last element in the heap
+		while (elements[--size_heap] >= elements[0]);
+		swap(elements[0], elements[size_heap]);				// the first element of the heap is now in the elements part
+		int idx = 0;
+		while (idx < size_heap) {
+			int left = idx * 2 + 1, right = idx * 2 + 2;	// indices of left and right children in the heap
+			if ((right < size_heap) && (elements[right] > elements[left]) && (elements[right] > elements[idx])) {
+				swap(elements[idx], elements[right]);
+				idx = right;
 			}
+			else if ((left < size_heap) && (elements[left] >elements[idx])) {
+				swap(elements[idx], elements[left]);
+				idx = left;
+			}
+			else
+				break;
 		}
-		for (int i = idx + 1; i < num; i++)
-			cout << " " << cur[i];
 	}
 	else {
-		cout << "Heap Sort" << endl;
-		int max_idx = num;
-		while (cur[0] < cur[--max_idx]);
-		cur[0] ^= cur[max_idx];
-		cur[max_idx] ^= cur[0];
-		cur[0] ^= cur[max_idx];
-		max_idx--;
-		int cur_idx = 0;
-		while ((cur_idx << 1) + 1 <= max_idx) {
-			int idx = (cur_idx << 1) + 1;
-			if ((idx < max_idx) && (cur[idx] < cur[idx + 1]))
-				idx++;
-			cur[cur_idx] ^= cur[idx];
-			cur[idx] ^= cur[cur_idx];
-			cur[cur_idx] ^= cur[idx];
-			cur_idx = idx;
-		}
-		cout << cur[0];
-		for (int i = 1; i < num; i++)
-			cout << " " << cur[i];
+		printf("Insertion Sort\n");
+		int idx = 1;										// index of the first unsorted element
+		while (elements[idx] >= elements[idx - 1])
+			idx++;
+		sort(elements, elements + idx + 1);					// sort the first idx + 1 elements
 	}
+	printf("%d", elements[0]);
+	for (int i = 1; i < num_element; i++)
+		printf(" %d", elements[i]);
 
 	return 0;
 }

cf5d27bbe755b2bbe72fae694ea2d574dabb5600
Mourzoufle <Mourzoufle@163.com>
2016-04-11 00:23:42 +0800

    1100 - final

--- 1100.cc
+++ 1100.cc
@@ -1,58 +1,80 @@
-#include <iostream>
-#include <string>
-#include <sstream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
 
-using namespace std;
-
-string nums[][13] = {
-	"tret", "jan", "feb", "mar", "apr", "may", "jun", "jly", "aug", "sep", "oct", "nov", "dec",
-	"tret", "tam", "hel", "maa", "huh", "tou", "kes", "hei", "elo", "syy", "lok", "mer", "jou"
+const char SPELLS[][8] = {
+	"tret",
+	"jan",
+	"feb",
+	"mar",
+	"apr",
+	"may",
+	"jun",
+	"jly",
+	"aug",
+	"sep",
+	"oct",
+	"nov",
+	"dec",
+	"tam",
+	"hel",
+	"maa",
+	"huh",
+	"tou",
+	"kes",
+	"hei",
+	"elo",
+	"syy",
+	"lok",
+	"mer",
+	"jou"
 };
 
 int main() {
-	int num;
-	cin >> num;
-	cin.ignore();
-	for (int i = 0; i < num; i++) {
-		string str;
-		getline(cin, str);
-		if ((str[0] >= '0') && (str[0] <= '9')) {
-			stringstream ss;
-			int value;
-			ss << str;
-			ss >> value;
-			if (value > 12) {
-				cout << nums[1][value / 13];
-				if (value % 13 > 0)
-					cout << " " << nums[0][value % 13];
-				cout << endl;
+	int num_num;									// number of given numbers
+	scanf("%d\n", &num_num);
+	for (int i = 0; i < num_num; i++) {
+		char str[16];
+		gets(str);
+		if ((str[0] >= '0') && (str[0] <= '9')) {	// an Earth number
+			int num = atoi(str);
+			if (num > 12) {							// has higher digit
+				printf("%s", SPELLS[num / 13 + 12]);
+				if (num % 13 > 0)
+					printf(" %s", SPELLS[num % 13]);
+				printf("\n");
 			}
 			else
-				cout << nums[0][value % 13] << endl;
+				printf("%s\n", SPELLS[num]);
 		}
-		else {
-			int value = 0;
-			int idx = str.find(" ");
-			if (idx != string::npos) {
-				for (int j = 0; j < 13; j++) {
-					if (nums[1][j] == str.substr(0, idx)) {
-						value = j * 13;
+		else {										// a Mars number
+			int num = 0;
+			if (strlen(str) > 4) {					// the number contains two spells
+				int idx = -1;
+				while (str[++idx] != ' ');
+				str[idx] = '\0';
+				for (int i = 13; i < 25; i++) {
+					if (strcmp(str, SPELLS[i]) == 0) {
+						num = (i - 12) * 13;
 						break;
 					}
 				}
-				str = str.substr(idx + 1);
-			}
-			for (int j = 0; j < 13; j++) {
-				if (nums[0][j] == str) {
-					value += j;
-					break;
-				}
-				if (nums[1][j] == str) {
-					value = j * 13;
-					break;
+				for (int i = 0; i < 13; i++) {
+					if (strcmp(str + idx + 1, SPELLS[i]) == 0) {
+						num += i;
+						break;
+					}
 				}
 			}
-			cout << value << endl;
+			else {									// the number contains only one spell
+				for (int i = 0; i < 25; i++) {
+					if (strcmp(str, SPELLS[i]) == 0) {
+						num = (i > 13 ? (i - 12) * 13 : i);
+						break;
+					}
+				}
+			}
+			printf("%d\n", num);
 		}
 	}
 

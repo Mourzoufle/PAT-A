1493706d67a0e5d8b67ab1f6d95d459ad643b43c
Mourzoufle <Mourzoufle@163.com>
2016-04-10 14:41:03 +0800

    1099 - final

--- 1099.cc
+++ 1099.cc
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <cstdio>
 #include <stack>
 #include <queue>
 #include <algorithm>
@@ -13,41 +13,40 @@ struct Node {
 
 int main() {
 	int num_node;
-	cin >> num_node;
+	scanf("%d", &num_node);
 	Node *nodes = new Node[num_node];
 	for (int i = 0; i < num_node; i++)
-		cin >> nodes[i].left >> nodes[i].right;
+		scanf("%d %d", &nodes[i].left, &nodes[i].right);
 	int *keys = new int[num_node];
 	for (int i = 0; i < num_node; i++)
-		cin >> keys[i];
+		scanf("%d", &keys[i]);
 	sort(keys, keys + num_node);
 
-	stack<int> stack;
-	int cur = 0;
+	stack<int> stack;	// inorder traversal
+	int cur_node = 0;	// current node to be visited
 	for (int i = 0; i < num_node; i++) {
-		while (cur >= 0) {
-			stack.push(cur);
-			cur = nodes[cur].left;
+		while (cur_node >= 0) {
+			stack.push(cur_node);
+			cur_node = nodes[cur_node].left;
 		}
-		cur = stack.top();
+		cur_node = stack.top();
+		nodes[cur_node].key = keys[i];
+		cur_node = nodes[cur_node].right;
 		stack.pop();
-		nodes[cur].key = keys[i];
-		cur = nodes[cur].right;
 	}
 
-	queue<int> queue;
+	queue<int> queue;	// level order traversal
 	queue.push(0);
-	while (!queue.empty()) {
-		int idx = queue.front();
+	for (int i = 0; i < num_node - 1; i++) {
+		int node = queue.front();
+		printf("%d ", nodes[node].key);
+		if (nodes[node].left >= 0)
+			queue.push(nodes[node].left);
+		if (nodes[node].right >= 0)
+			queue.push(nodes[node].right);
 		queue.pop();
-		if (nodes[idx].left >= 0)
-			queue.push(nodes[idx].left);
-		if (nodes[idx].right >= 0)
-			queue.push(nodes[idx].right);
-		cout << nodes[idx].key;
-		if (!queue.empty())
-			cout << " ";
 	}
+	printf("%d", nodes[queue.front()].key);
 
 	return 0;
 }

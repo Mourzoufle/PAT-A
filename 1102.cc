38230f21e752f2a61a6d9182a676b2305ac3a7ab
Mourzoufle <Mourzoufle@163.com>
2016-04-12 00:04:29 +0800

    1102 - final

--- 1102.cc
+++ 1102.cc
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <cstdio>
 #include <queue>
 #include <stack>
 
@@ -11,21 +11,22 @@ struct Node {
 
 int main() {
 	int num_node;
-	cin >> num_node;
+	scanf("%d", &num_node);
 	Node *nodes = new Node[num_node];
-	bool *is_root = new bool[num_node];
+	bool *is_root = new bool[num_node];								// flag indicating the root
 	for (int i = 0; i < num_node; i++)
 		is_root[i] = true;
 	for (int i = 0; i < num_node; i++) {
-		string left, right;
-		cin >> left >> right;
-		nodes[i].right = (left == "-" ? -1 : left[0] - '0');
-		nodes[i].left = (right == "-" ? -1 : right[0] - '0');
+		char left[4], right[4];
+		scanf("%s %s", &left, &right);
+		nodes[i].right = (left[0] == '-' ? -1 : left[0] - '0');		// swap the left and right children
+		nodes[i].left = (right[0] == '-' ? -1 : right[0] - '0');
 		if (nodes[i].left >= 0)
 			is_root[nodes[i].left] = false;
 		if (nodes[i].right >= 0)
 			is_root[nodes[i].right] = false;
 	}
+
 	int root;
 	for (int i = 0; i < num_node; i++) {
 		if (is_root[i]) {
@@ -34,35 +35,34 @@ int main() {
 		}
 	}
 
-	queue<int> queue;
+	queue<int> queue;												// level-order traversal
 	queue.push(root);
 	while (!queue.empty()) {
-		int cur = queue.front();
+		int node = queue.front();
+		if (nodes[node].left >= 0)
+			queue.push(nodes[node].left);
+		if (nodes[node].right >= 0)
+			queue.push(nodes[node].right);
+		printf("%d", node);
 		queue.pop();
-		if (nodes[cur].left >= 0)
-			queue.push(nodes[cur].left);
-		if (nodes[cur].right >= 0)
-			queue.push(nodes[cur].right);
-		cout << cur;
 		if (!queue.empty())
-			cout << " ";
-		else
-			cout << endl;
+			printf(" ");
 	}
+	printf("\n");
 
-	stack<int> stack;
-	int cur = root;
+	stack<int> stack;												// in-order traversal
+	int cur_node = root;
 	for (int i = 0; i < num_node; i++) {
-		while (cur >= 0) {
-			stack.push(cur);
-			cur = nodes[cur].left;
-		}
-		cur = stack.top();
-		stack.pop();
 		if (i > 0)
-			cout << " ";
-		cout << cur;
-		cur = nodes[cur].right;
+			printf(" ");
+		while (cur_node >= 0) {
+			stack.push(cur_node);
+			cur_node = nodes[cur_node].left;
+		}
+		cur_node = stack.top();
+		stack.pop();
+		printf("%d", cur_node);
+		cur_node = nodes[cur_node].right;
 	}
 
 	return 0;

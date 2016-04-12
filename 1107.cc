37fe82719f424faf5d0487bd602ab40084532f23
Mourzoufle <Mourzoufle@163.com>
2016-04-13 00:08:18 +0800

    1107 - final

--- 1107.cc
+++ 1107.cc
@@ -7,9 +7,9 @@ using namespace std;
 const int MAX_HOBBY = 1000;
 
 struct Cluster {
-	int root;
-	int num_member;
-	bool hobbies[MAX_HOBBY];
+	int root;								// root member of the cluster
+	int num_member;							// number of members
+	bool hobbies[MAX_HOBBY];				// flag indicating if this member has each hobby 
 
 	Cluster(): num_member(1) {
 		for (int i = 0; i < MAX_HOBBY; i++)
@@ -17,15 +17,16 @@ struct Cluster {
 	}
 };
 
+/* Union-Find Sets - merge the sets that a and b belong to */
 void merge(Cluster *clusters, int a, int b) {
 	int root_a = a, root_b = b;
 	while (clusters[root_a].root != root_a)
 		root_a = clusters[root_a].root;
 	while (clusters[root_b].root != root_b)
 		root_b = clusters[root_b].root;
-	if (root_a != root_b)
+	if (root_a != root_b)					// merge two different clusters and update the number of members
 		clusters[root_a].num_member += clusters[root_b].num_member;
-	while (clusters[a].root != root_a) {
+	while (clusters[a].root != root_a) {	// path compression of a and b
 		int tmp = clusters[a].root;
 		clusters[a].root = root_a;
 		a = tmp;
@@ -54,7 +55,7 @@ int main() {
 
 	for (int i = 0; i < num_person; i++) {
 		for (int j = i + 1; j < num_person; j++) {
-			bool share = false;
+			bool share = false;				// flag indicating if i and j share the same hobby
 			for (int k = 0; k < MAX_HOBBY; k++) {
 				if (clusters[i].hobbies[k] && clusters[j].hobbies[k]) {
 					share = true;
@@ -66,7 +67,7 @@ int main() {
 		}
 	}
 
-	vector<int> nums_member;
+	vector<int> nums_member;				// number of the members in each cluster
 	for (int i = 0; i < num_person; i++)
 		if (clusters[i].root == i)
 			nums_member.push_back(clusters[i].num_member);

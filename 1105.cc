dfc0ce960bfa62f68f3d9b845c53e82da36f0a08
Mourzoufle <Mourzoufle@163.com>
2016-04-12 23:46:38 +0800

    1105 - final

--- 1105.cc
+++ 1105.cc
@@ -5,14 +5,14 @@
 using namespace std;
 
 int main() {
-	int num_num;
+	int num_num;											// number of numbers
 	scanf("%d", &num_num);
 	int *nums = new int[num_num];
 	for (int i = 0; i < num_num; i++)
 		scanf("%d", &nums[i]);
 	sort(nums, nums + num_num);
 
-	int num_row, num_col;
+	int num_row, num_col;									// number of rows and columns
 	for (int i = sqrt(num_num - 1) + 1; i <= num_num; i++) {
 		if (num_num % i == 0) {
 			num_row = i;
@@ -26,8 +26,7 @@ int main() {
 		for (int j = 0; j < num_col; j++)
 			matrix[i][j] = 0;
 	}
-
-	int cur_row = 0, cur_col = 0, inc_row = 0, inc_col = 1;
+	int cur_row = 0, cur_col = 0, inc_row = 0, inc_col = 1;	// current row and column, and the increment of them
 	matrix[0][0] = nums[num_num - 1];
 	for (int i = num_num - 2; i >= 0; i--) {
 		if ((inc_row > 0) && ((cur_row + inc_row >= num_row) || (matrix[cur_row + inc_row][cur_col] > 0))) {

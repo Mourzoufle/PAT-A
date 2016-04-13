27127c03306cab9bd482943bc5e82d2410be3f2c
Mourzoufle <Mourzoufle@163.com>
2016-04-13 20:46:51 +0800

    1109 - final

--- 1109.cc
+++ 1109.cc
@@ -9,6 +9,7 @@ struct Person {
 	int height;
 };
 
+/* compare function for sorting people - first according to their heights, then their names if there is a tie */
 bool cmp(const Person &a, const Person &b) {
 	if (a.height != b.height)
 		return a.height > b.height;
@@ -24,15 +25,15 @@ int main() {
 		scanf("%s %d", people[i].name, &people[i].height);
 	sort(people, people + num_person, cmp);
 
-	int idx_head = 0;
+	int idx_head = 0;	// the first person that has not been placed
 	for (int i = 0; i < num_row; i++) {
 		int cur_size = num_col;
 		if (i == 0)
 			cur_size += num_person % num_row;
-		for (int j = idx_head + (cur_size / 2) * 2 - 1; j > idx_head; j -= 2)
+		for (int j = idx_head + (cur_size / 2) * 2 - 1; j > idx_head; j -= 2)	// left to right: head + 2k + 1, ..., head + 1
 			printf("%s ", people[j].name);
-		printf("%s", people[idx_head].name);
-		for (int j = idx_head + 2; j < idx_head + cur_size; j += 2)
+		printf("%s", people[idx_head].name);									// ...head
+		for (int j = idx_head + 2; j < idx_head + cur_size; j += 2)				// ...head + 2, ..., head + 2k
 			printf(" %s", people[j].name);
 		printf("\n");
 		idx_head += cur_size;

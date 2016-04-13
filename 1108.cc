161df05d4ead65d22a8e6fd7733dccd53e483d7f
Mourzoufle <Mourzoufle@163.com>
2016-04-13 20:40:31 +0800

    1108 - final

--- 1108.cc
+++ 1108.cc
@@ -2,32 +2,33 @@
 #include <cstring>
 #include <cstdlib>
 
-const int MAX_LENGTH = 1 << 16;
+const int MAX_LENGTH = 64;								// assume each length of input number is no larger than this
 
+/* check if the string representation of a number is valid, and convert it to double */
 bool is_valid(char *str, double &num) {
 	int idx_dot = -1;
 	for (int i = 0; str[i] != '\0'; i++) {
 		if (str[i] == '-') {
-			if (i > 0)
+			if (i > 0)									// the minus sign should appear only in the head of the string
 				return false;
 		}
 		else if (str[i] == '.') {
-			if (idx_dot < 0)
+			if (idx_dot < 0)							// no dot has been detected
 				idx_dot = i;
 			else
 				return false;
 		}
-		else if ((str[i] < '0') || (str[i] > '9'))
+		else if ((str[i] < '0') || (str[i] > '9'))		// illegal character
 			return false;
 	}
-	if ((idx_dot >= 0) && (strlen(str) - idx_dot) > 3)
+	if ((idx_dot >= 0) && (strlen(str) - idx_dot) > 3)	// too many significant digits
 		return false;
 	num = atof(str);
 	return (num >= -1000) && (num <= 1000);
 }
 
 int main() {
-	int num_num, num_valid = 0;
+	int num_num, num_valid = 0;							// number of given numbers; number of valid ones among them
 	double sum = 0;
 	scanf("%d", &num_num);
 	for (int i = 0; i < num_num; i++) {

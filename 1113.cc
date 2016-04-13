98f7a8baa6494b9628934ce489c4be181dd0f484
Mourzoufle <Mourzoufle@163.com>
2016-04-14 00:25:41 +0800

    1113 - final

--- 1113.cc
+++ 1113.cc
@@ -4,14 +4,14 @@
 using namespace std;
 
 int main() {
-	int num_num;
+	int num_num;	// number of numbers in the set
 	scanf("%d", &num_num);
 	int *nums = new int[num_num];
 	for (int i = 0; i < num_num; i++)
 		scanf("%d", &nums[i]);
 	sort(nums, nums + num_num);
 
-	int diff = 0;
+	int diff = 0;	// the difference - the max one is between the smaller half to the rest
 	for (int i = 0; i < num_num / 2; i++)
 		diff -= nums[i];
 	for (int i = num_num / 2; i < num_num; i++)
